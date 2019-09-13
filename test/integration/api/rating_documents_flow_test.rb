d4a05b72b1dd33d73b936c44d53ff5531fa80054
epugh <epugh@opensourceconnections.com>
2019-09-13 15:40:27 -0400

    test now reflects we arent fixing this via api, but via client and base64

--- test/integration/api/rating_documents_flow_test.rb
+++ test/integration/api/rating_documents_flow_test.rb
@@ -16,10 +16,11 @@ class RatingDocumentsFlowTest < ActionDispatch::IntegrationTest
       { controller: "api/v1/queries/ratings", action: "update", format: :json, case_id: "44", "query_id": "62", doc_id: "mydoc" }
     )
 
-    # a period in the URL should work as well, and not be truncated.
+    # A period in the doc_id should work, however Rails assumes that post the dot is the format type.   We deal with this by
+    # Base64 encoding in the Angular front end and decoding in ratings controller.
     assert_routing(
       { method: 'put', path: "/api/cases/44/queries/62/ratings/mydoc.pdf" },
-      { controller: "api/v1/queries/ratings", action: "update", format: :json, case_id: "44", "query_id": "62", doc_id: "mydoc.pdf" }
+      { controller: "api/v1/queries/ratings", action: "update", "format": "pdf", case_id: "44", "query_id": "62", doc_id: "mydoc" }
     )
   end
 end
