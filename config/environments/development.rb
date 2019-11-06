bc10e8a88a5bbed9b1fcc3f89dfea2b37a3a6552
epugh <epugh@opensourceconnections.com>
2019-11-05 21:37:43 -0500

    get name of Quepid image to match what is in dockerhub

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   # config.assets.debug = true
-  config.assets.debug = false
+  config.assets.debug = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
