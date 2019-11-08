c1dfb5c580df74305f5bd4cb4208969a46cdd148
epugh <epugh@opensourceconnections.com>
2019-11-08 16:23:36 -0500

    remove the no longer used support to resize components based on mobile device or not

--- app/assets/javascripts/components/user_listing/user_listing_controller.js
+++ app/assets/javascripts/components/user_listing/user_listing_controller.js
@@ -9,19 +9,7 @@ angular.module('QuepidApp')
       var ctrl = this;
 
       ctrl.user  = $scope.user;
-      ctrl.light = $scope.light;
       ctrl.team  = $scope.team;
 
-      if ( ctrl.light ) {
-        ctrl.colWidth = {
-          image:  'col-xs-3 col-md-1',
-          name:   'col-xs-9 col-md-11',
-        };
-      } else {
-        ctrl.colWidth = {
-          image:  'col-xs-3 col-md-1',
-          name:   'col-xs-9 col-md-11',
-        };
-      }
     }
   ]);
