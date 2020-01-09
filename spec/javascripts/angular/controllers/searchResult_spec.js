dd5ff8581f0c69cd1c0b6c3b565d943496df5341
epugh <epugh@opensourceconnections.com>
2020-01-09 06:48:35 -0500

    added hacky tests, and tweaked ui

--- spec/javascripts/angular/controllers/searchResult_spec.js
+++ spec/javascripts/angular/controllers/searchResult_spec.js
@@ -17,22 +17,49 @@ describe('Controller: SearchResultCtrl', function () {
   //  });
   //}));
 
-  it('rejects empty or blank text', function() {
+  it('properly detects a field with leading http or www text', function() {
 
     // This method should actually be on the SearchResultCtrl (searchResult.js), however
     // we don't know how to make this test work.  So leaving this way for now.
-    }
+
     var isUrl = function(value) {
-      return (/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(value.trim()));
+      return (/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm.test(value));
     };
 
     expect(isUrl("http://www.example.com")).toBe(true);
     expect(isUrl("https://www.example.com/blah")).toBe(true);
     expect(isUrl("some text with nested https://www.example.com/blah shouldnt be true")).toBe(false);
-    expect(isUrl("  https://www.example.com/blah")).toBe(true);
-    expect(isUrl("  https://www.example.com/blah")).toBe(true);
+    expect(isUrl("  https://www.example.com/blah")).toBe(false);
+    expect(isUrl("  https://www.example.com/blah")).toBe(false);
     expect(isUrl("www.example.com/blah")).toBe(true);
 
+  });
+
+  it('properly detects if we have a JSON object', function() {
+
+    // This method should actually be on the SearchResultCtrl (searchResult.js), however
+    // we don't know how to make this test work.  So leaving this way for now.
+
+    var isJSONObject = function(value) {
+      //return typeof value === 'object' &&  value instanceof Array !== true;
+      if (typeof value === 'object' && value instanceof Array === true) {
+        return typeof value[0] === 'object'
+      }
+      else if (typeof value === 'object' &&  value instanceof Array !== true) {
+        return true;
+      }
+      return false;
+    };
+
+
+    var json_obj = { name: "Drama", id: 18};
+    var string_array = [ "some array" ];
+    var json_array = [ { name: "Drama", id: 18}, { name: "Horror", id: 3} ]
+
+    expect(isJSONObject("hello world")).toBe(false);
+    expect(isJSONObject(json_obj)).toBe(true);
+    expect(isJSONObject(string_array)).toBe(false);
+    expect(isJSONObject(json_array)).toBe(true);
   });
 
 });
