72f381e33657d81ef95844d88443decf6ca9394d
Daniel Worley <danworley@gmail.com>
2020-01-15 08:41:44 -0500

    Feature/media embeds (#62)
    * Issue #56: Media field support
    
    * Embed fixes, style cleanup
    
    * Update splainer version

--- app/assets/javascripts/directives/searchResult.js
+++ app/assets/javascripts/directives/searchResult.js
@@ -39,3 +39,61 @@ angular.module('QuepidApp')
       };
     }
   ]);
+
+angular.module('QuepidApp')
+  .directive('quepidEmbed', [
+    function() {
+      function isAudio(val) {
+        var audioExtensions = [
+            '.mp3',
+            '.wav',
+            '.ogg'
+        ];
+
+        return (new RegExp(audioExtensions.join('|')).test(val));
+      }
+
+      function isImage(val) {
+        var imageExtensions = [
+            '.jpg',
+            '.jpeg',
+            '.gif',
+            '.png'
+        ];
+
+        return (new RegExp(imageExtensions.join('|')).test(val));
+      }
+
+      function isVideo(val) {
+        var videoExtensions = [
+            '.mp4',
+            '.webm'
+        ];
+
+        return (new RegExp(videoExtensions.join('|')).test(val));
+      }
+
+      return {
+        restrict: 'A',
+        scope: {
+          src: '='
+        },
+        templateUrl: 'views/embed.html',
+        link: function(scope) {
+            // Init vars
+            scope.audioSrc = scope.imageSrc = scope.videoSrc = null;
+
+            // Audio Embed
+            if (isAudio(scope.src)) {
+                scope.audioSrc = scope.src;
+            // Image Embed
+            } else if (isImage(scope.src)) {
+                scope.imageSrc = scope.src;
+            // Video Embed
+            } else if(isVideo(scope.src)) {
+                scope.videoSrc = scope.src;
+            }
+        }
+      };
+    }
+  ]);
