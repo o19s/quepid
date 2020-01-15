a79ae6162dde37cfbf418e80912a3a5492649214
epugh <epugh@opensourceconnections.com>
2020-01-14 20:09:32 -0500

    one more

--- app/assets/javascripts/services/settingsSvc.js
+++ app/assets/javascripts/services/settingsSvc.js
@@ -109,7 +109,7 @@ angular.module('QuepidApp')
             tryToUse = settings.selectedTry;
             settings.selectTry(tryToUse.tryNo);
           }
-          console.log("settingsSvc: editableSettings setting up tryToUse");
+          console.log('settingsSvc: editableSettings setting up tryToUse');
           console.log(tryToUse);
           settings.escapeQuery   = tryToUse.escapeQuery;
           settings.fieldSpec     = tryToUse.fieldSpec;
