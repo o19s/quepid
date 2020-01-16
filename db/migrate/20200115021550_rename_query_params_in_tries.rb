d9ce2571798804097619c25219494ca1810591bf
epugh <epugh@opensourceconnections.com>
2020-01-16 14:29:59 -0500

    had migration fail due to odd characters, but it worked when manually run in database, so doc steps

--- db/migrate/20200115021550_rename_query_params_in_tries.rb
+++ db/migrate/20200115021550_rename_query_params_in_tries.rb
@@ -1,5 +1,9 @@
 class RenameQueryParamsInTries < ActiveRecord::Migration
   def change
+    # Note, this failed once on werid data in queryParams, but manually running worked:
+    # ALTER TABLE `tries` CHANGE `queryParams` `query_params` varchar(20000) DEFAULT NULL
+    # Then skip the failed migration by inserting a fake record:
+    # INSERT INTO schema_migrations (version) VALUES (20200115021550)
     rename_column :tries, :queryParams, :query_params
   end
 end
