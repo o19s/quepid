a359f54f72d97a4bfc58355dcea149023fc17f12
Nathan Day <nathancday@gmail.com>
2020-03-20 11:46:15 -0400

    dcg scorer

---
+++ db/scorers/dcg@5.js
@@ -0,0 +1,10 @@
+var k = 5 // @Rank
+var score = 0;
+
+eachDoc(function(doc, i) {
+    var d = Math.log2(i+2); // i is the JSindex not the DocRank; 0 vs 1
+    var n = Math.pow(2,docRating(i))-1; 
+    score += d?(n/d):0;
+}, k)
+
+setScore(score);
