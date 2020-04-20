96b49e76058450f2983a2049deb78d0adc6af162
epugh <epugh@opensourceconnections.com>
2020-04-20 12:18:38 -0400

    get bullet to quick yelling at us with alerts! down boy down!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,10 +52,9 @@ Rails.application.configure do
 
   config.after_initialize do
     Bullet.enable = true
-    Bullet.alert = true
+    Bullet.alert = false
     Bullet.bullet_logger = true
     Bullet.console = true
-    #  Bullet.growl = true
     Bullet.rails_logger = true
     Bullet.add_footer = true
   end
