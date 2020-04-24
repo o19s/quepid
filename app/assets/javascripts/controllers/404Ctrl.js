a378efd7b7de5480e88d2cf63c41a693fd772d3e
epugh <epugh@opensourceconnections.com>
2020-04-24 15:09:31 -0400

    someone learning for the first time ;-) https://www.sitepoint.com/kickstart-your-angularjs-development-with-yeoman-grunt-and-bower/

--- app/assets/javascripts/controllers/404Ctrl.js
+++ app/assets/javascripts/controllers/404Ctrl.js
@@ -4,10 +4,6 @@ angular.module('QuepidApp')
   .controller('404Ctrl', [
     '$scope',
     function ($scope) {
-      $scope.awesomeThings = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
-      ];
+    
     }
   ]);
