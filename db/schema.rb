18ac65d6c28b4292fc7b97d56e7b9a3ed1151320
epugh <epugh@opensourceconnections.com>
2020-04-28 11:10:42 -0400

    fix #104 by updating existing cases

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200413184942) do
+ActiveRecord::Schema.define(version: 20200428150211) do
 
   create_table "annotations", force: :cascade do |t|
     t.text     "message",    limit: 65535
