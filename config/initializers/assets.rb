1096ba4ab085bcf0ec26120b247f94adb42c34ad
epugh <epugh@opensourceconnections.com>
2020-05-08 14:54:17 -0400

    only compile karma if we are in test mode

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,7 +10,7 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.paths << Rails.root.join('spec', 'karma') if Rails.env.development? || Rails.env.test?
+Rails.application.config.assets.paths << Rails.root.join('spec', 'karma') if Rails.env.test?
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
