b9b40c82ffec45d041bdc60c3aca8cf05c5c46b0
epugh <epugh@opensourceconnections.com>
2020-05-09 15:03:21 -0400

    not sure why but conflict with the darn sidekiq admin app and bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,11 +51,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['QUEPID_DOMAIN'], port: ENV['PORT'] }
 
   config.after_initialize do
-    Bullet.enable = true
-    Bullet.alert = false
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.rails_logger = true
-    Bullet.add_footer = true
+    #Bullet.enable = true
+    #Bullet.alert = false
+    #Bullet.bullet_logger = true
+    #Bullet.console = true
+    #Bullet.rails_logger = true
+    #Bullet.add_footer = true
   end
 end
