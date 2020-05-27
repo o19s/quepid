ee4cd5a0c7cd605a3cf3bd9241d74440ab4fe36f
epugh <epugh@opensourceconnections.com>
2020-05-27 08:28:22 -0400

    doc somewhere why we are leaving the default_scorers table for now

--- db/migrate/20200522215022_remove_scorer_type.rb
+++ db/migrate/20200522215022_remove_scorer_type.rb
@@ -1,4 +1,9 @@
 class RemoveScorerType < ActiveRecord::Migration
+  # Note: we are leaving the default_scorer table alone for now in case
+  # we need to bring it back, we don't want to lose the data.  Just in case..
+  when condition
+
+  end
   def change
     remove_column :cases,   :scorer_type
     remove_column :queries, :scorer_type
