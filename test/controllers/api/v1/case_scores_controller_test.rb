975e6d61f8e018c359cea30e3642a942f68ddd51
epugh <epugh@opensourceconnections.com>
2020-07-09 13:11:48 -0400

    one more renaming

--- test/controllers/api/v1/case_scores_controller_test.rb
+++ test/controllers/api/v1/case_scores_controller_test.rb
@@ -18,7 +18,7 @@ module Api
 
       describe 'Updates case score' do
         test 'return an error if try id is not specified' do
-          put :update, case_id: acase.id, score: { score: 1 }
+          put :update, case_id: acase.id, case_score: { score: 1 }
 
           assert_response :bad_request
         end
