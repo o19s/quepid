99fc10cc0c614334499ac4fd0cc904be600376bc
Eric Pugh <epugh@opensourceconnections.com>
2020-09-11 16:09:27 -0400

    Default to ten docs deep scoring (#209)
    * and we have made the default depth change
    
    * maybe we ought to actually save our changes!

---
+++ db/migrate/20200911192850_change_default_scorers_to_at_ten_deep.rb
@@ -0,0 +1,21 @@
+class ChangeDefaultScorersToAtTenDeep < ActiveRecord::Migration
+  def change
+
+    scorers_to_update = ['P@5', 'AP@5','nDCG@5', 'DCG@5', 'CG@5']
+
+    scorers_to_update.each do |scorer_name|
+      scorer = Scorer.where(name: scorer_name).first
+      unless scorer.nil?
+        name = scorer.name
+        name.sub!('@5', '@10')
+        scorer.name = name
+        scorer.code = File.readlines("./db/scorers/#{name.downcase}.js",'\n').join('\n'),
+        scorer.save!
+      end
+
+
+    end
+
+
+  end
+end
