cbb2432d86e4fcc58d35874f9933dd745cb68a48
Eric Pugh <epugh@opensourceconnections.com>
2020-10-29 14:32:20 -0400

    Move "docid" from the URL to a JSON payload (#233)
    * swap JS side over to using payloads for doc ids
    
    * refactor to be more railsy is working

--- test/integration/api/rating_documents_flow_test.rb
+++ test/integration/api/rating_documents_flow_test.rb
@@ -3,46 +3,17 @@
 require 'test_helper'
 
 class RatingDocumentsFlowTest < ActionDispatch::IntegrationTest
-  it 'can rate documents that have various formatted document ids' do
-    # asserts the route for normal doc id's works.
+  it 'can rate documents where the doc_id isnt in the route' do
     assert_routing(
       {
         method: 'put',
-        path:   '/api/cases/44/queries/62/ratings/99',
+        path:   '/api/cases/44/queries/62/ratings',
       },
       controller: 'api/v1/queries/ratings',
       action:     'update',
       format:     :json,
       case_id:    '44',
-      'query_id': '62',
-      doc_id:     '99'
-    )
-
-    # make sure using other non numeric identifiers works.
-    assert_routing(
-      {
-        method: 'put',
-        path:   '/api/cases/44/queries/62/ratings/mydoc',
-      },
-      controller: 'api/v1/queries/ratings',
-      action:     'update',
-      format:     :json,
-      case_id:    '44',
-      'query_id': '62',
-      doc_id:     'mydoc'
-    )
-
-    # A period in the doc_id should work, however Rails assumes that post the dot is the format type.
-    # We deal with this by Base64 encoding in the Angular front end and decoding in ratings controller.
-    assert_routing(
-      {
-        method: 'put',
-        path:   '/api/cases/44/queries/62/ratings/mydoc.pdf',
-      },
-      controller: 'api/v1/queries/ratings',
-      action:     'update', 'format': 'pdf',
-      case_id:    '44', 'query_id': '62',
-      doc_id:     'mydoc'
+      query_id:   '62'
     )
   end
 end
