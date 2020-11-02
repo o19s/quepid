94dd23990422901082d79b121c1ca86a76907dc3
epugh <epugh@opensourceconnections.com>
2020-11-02 09:42:47 -0500

    Turns out we were updating a non communal ndcg scorer, hence not seeing the update for the communal

--- db/migrate/20200911192850_change_default_scorers_to_at_ten_deep.rb
+++ db/migrate/20200911192850_change_default_scorers_to_at_ten_deep.rb
@@ -4,7 +4,7 @@ class ChangeDefaultScorersToAtTenDeep < ActiveRecord::Migration
     scorers_to_update = ['P@5', 'AP@5','nDCG@5', 'DCG@5', 'CG@5']
 
     scorers_to_update.each do |scorer_name|
-      scorer = Scorer.where(name: scorer_name).first
+      scorer = Scorer.where(name: scorer_name, communal: true).first
       unless scorer.nil?
         name = scorer.name
         name.sub!('@5', '@10')
