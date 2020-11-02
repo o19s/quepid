19b046485db530162c213a593e5b2e9df8fbbf07
epugh <epugh@opensourceconnections.com>
2020-11-02 09:26:48 -0500

    hotfix: k=5 despite scores being changed to @10, i.e k should be 10

---
+++ db/migrate/20201102142255_redo_communal_scorers_missing_proper_k.rb
@@ -0,0 +1,16 @@
+# It appears that we still have k=5, even through the scores are labeled as @10, so fix this.
+class RedoCommunalScorersMissingProperK < ActiveRecord::Migration
+  def change
+
+    scorers_to_update = ['P@10', 'AP@10','nDCG@10', 'DCG@10', 'CG@10']
+
+    scorers_to_update.each do |scorer_name|
+      scorer = Scorer.where(name: scorer_name).first
+      unless scorer.nil?
+        name = scorer.name
+        scorer.code = File.readlines("./db/scorers/#{name.downcase}.js",'\n').join('\n'),
+        scorer.save!
+      end
+    end
+  end
+end
