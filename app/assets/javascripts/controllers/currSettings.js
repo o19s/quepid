d1a0e9e18b71aa39d7cc065f52fe3f86c6031927
epugh <epugh@opensourceconnections.com>
2020-11-14 16:30:55 -0500

    went ahead and made the try editable as well

--- app/assets/javascripts/controllers/currSettings.js
+++ app/assets/javascripts/controllers/currSettings.js
@@ -18,5 +18,25 @@ angular.module('QuepidApp')
           return { name: '' };
         }
       }
+
+      $scope.tryName = {
+        name: null,
+        startRename: false,
+        rename: function() {
+          settingsSvc.renameTry($scope.currentTry.selectedTry().tryNo, $scope.tryName.name)
+          .then(function() {
+            $scope.tryName.startRename = false;
+          });
+        },
+        cancel: function() {
+          $scope.tryName.startRename = false;
+          $scope.tryName.name = $scope.currentTry.selectedTry().name;
+        }
+      };
+
+      $scope.tryNameEditModeToggle = function(){
+        $scope.tryName.name = $scope.currentTry.selectedTry().name;
+        $scope.tryName.startRename = !$scope.tryName.startRename;
+      };
     }
   ]);
