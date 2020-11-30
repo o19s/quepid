1331c68a2e176779c9430b77c5a462e71f6a8dfa
Daniel Worley <danworley@gmail.com>
2020-11-30 17:46:14 -0500

    Force deftype for explain missing lookups

--- app/assets/javascripts/controllers/docFinder.js
+++ app/assets/javascripts/controllers/docFinder.js
@@ -104,6 +104,8 @@ angular.module('QuepidApp')
         var settings      = settingsSvc.editableSettings();
         var fieldSpec     = settings.createFieldSpec();
 
+
+        $scope.docFinder.searcher.args['defType'] = 'lucene';
         $scope.docFinder.searcher.explainOther(
           $scope.query.filterToRatings(currSettings, $scope.docFinder.docs.length), fieldSpec)
         .then(function() {
@@ -161,6 +163,9 @@ angular.module('QuepidApp')
       $scope.docFinder.searcher = queriesSvc.createSearcherFromSettings(currSettings, $scope.query.queryText);
 
       if ($scope.docFinder.searcher.type === 'solr') {
+        // Force defType to lucene for ranked lookups
+        $scope.docFinder.searcher.args['defType'] = 'lucene';
+
         $scope.docFinder.searcher.explainOther(
           $scope.query.filterToRatings(currSettings, $scope.docFinder.docs.length), fieldSpec)
           .then(function() {
