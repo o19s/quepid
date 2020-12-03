a212f8435ffe1e60c0cf9737230469120cd0ccb2
Nathan (Nate) Day <nathancday@gmail.com>
2020-12-03 16:39:29 -0500

    Issue 225 (#253)
    * patch up nDCG logic for less than 10 result to fix #234
    
    * fix #225 for AP@10 being inccorect if less than 10 docs are returned
    
    * mergin
    
    * one more fix
    
    Co-authored-by: epugh@opensourceconnections.com <>

--- db/scorers/ap@10.js
+++ db/scorers/ap@10.js
@@ -1,5 +1,8 @@
 var k = 10; // @Rank
-total = 0
+total = 0;
+
+// if less than K results, need to reduce K now or final score is too low
+k = numReturned() < k ? numReturned() : k
 
 eachDoc(function(doc, i) {
     total += avgRating(i+1)
--- db/scorers/ndcg@10.js
+++ db/scorers/ndcg@10.js
@@ -1,9 +1,18 @@
 var k = 10 // @Rank
+var missing_rating = 0; // pessimistic assumption
 
-var ideal = topRatings(k)
+var ideal = topRatings(k) // could return less than k if less than k docs have ratings
+var missing_pad = Array(k - ideal.length).fill(missing_rating);
+ideal = ideal.concat(missing_pad);
 
-k = ideal.length < k ? ideal.length : k
-var scores = Array(k).fill(0);
+var scores = Array(k);
+eachDoc(function (doc, i) {
+  if (hasDocRating(i)) {
+    scores[i] = (docRating(i));
+  } else {
+      scores[i] = missing_rating;
+  }
+}, k)
 
 function DCG(vals, k) {
   var dcg = 0;
@@ -15,17 +24,11 @@ function DCG(vals, k) {
   return dcg;
 }
 
-function nDCG(vals, k) {
-  var ideal = topRatings(k)
+function nDCG(vals, ideal, k) {
   var n = DCG(vals, k);
   var d = DCG(ideal, k);
   return d ? (n / d) : 0;
 }
 
-eachDoc(function (doc, i) {
-  if (hasDocRating(i)) {
-    scores[i] = (docRating(i));
-  }
-}, k)
+setScore(nDCG(scores, ideal, k));
 
-setScore(nDCG(scores, k));
