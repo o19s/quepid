90c1c7bd56c289896e27f176d16e49f26b48aa97
epugh@opensourceconnections.com <>
2020-12-07 06:42:46 -0500

    fix up ids

--- spec/puppeteer/create_account.js
+++ spec/puppeteer/create_account.js
@@ -1,8 +1,8 @@
 
 const chromeOptions = {
   headless:false,
-  devtools: false,
-  slowMo:50,
+  devtools: true,
+  slowMo:10,
   defaultViewport:{
     width:1000,
     height:1500
@@ -31,37 +31,47 @@ const puppeteer = require('puppeteer');
 
     await navigationPromise
 
-    await page.type('#signup #name', 'Eric Pugh')
-    await page.type('#signup #email', email)
-    await page.type('#signup #password', password)
-    await page.type('#signup #password-confirm', password)
+    await page.type('#signup .name', 'Eric Pugh')
+    await page.type('#signup .email', email)
+    await page.type('#signup .password', password)
+    await page.type('#signup .password-confirm', password)
 
 
     await Promise.all([
-          await page.click('#signup #signup'),
+          await page.click('#signup .signup'),
           page.waitForNavigation({ waitUntil: 'networkidle0' })
     ]);
 
-    await page.waitForSelector('#step-one')
-    await page.waitFor(1000) // not sure why we have this.
-    await page.click(`#wizard #step-one #continue`)
+    await page.waitFor(4000) // not sure why we have this.
+
+    await page.evaluate(() => {
+      //debugger;
+    });
+
+
+    await page.click(`#wizard #step-one .continue`)
+
+    await page.evaluate(() => {
+      //debugger;
+      console.log("wating for step two")
+    });
 
     await page.waitForSelector('#step-two')
-    await page.click(`#wizard #step-two #continue`)
+    await page.click(`#wizard #step-two .continue`)
 
     await page.waitForSelector('#step-three')
-    await page.click('#wizard #step-three #continue')
+    await page.click('#wizard #step-three .continue')
 
     await page.waitFor(4000) // not sure why we have this.
 
-    await page.waitForSelector('#step-four #continue')
-    await page.click('#wizard #step-four #continue')
+    await page.waitForSelector('#step-four .continue')
+    await page.click('#wizard #step-four .continue')
 
     await page.waitForSelector('#step-five')
-    await page.click(`#wizard #step-five #continue`)
+    await page.click(`#wizard #step-five .continue`)
 
     await page.waitForSelector('#step-six')
-    await page.click(`#wizard #step-six #finish`)
+    await page.click(`#wizard #step-six .finish`)
 
 
     await page.waitForSelector('.shepherd-button-example-primary')
@@ -76,9 +86,24 @@ const puppeteer = require('puppeteer');
 
     await page.waitForSelector('div[rank="1"]')
 
-    await page.waitForSelector('.query-rating')
+    await page.click('div[rank="1"] .results-title')
+    //await page.waitForSelector('.query-rating')
+    await page.evaluate(() => {
+      debugger;
+    });
+    await page.waitFor(4000) // not sure why we have this.
+
+    //await page.click(`.shepherd-button-example-primary`)
+    await page.click('body > div.shepherd-step.shepherd-element.shepherd-open.shepherd-theme-arrows.shepherd-has-title.shepherd-has-cancel-link.shepherd-element-attached-bottom.shepherd-element-attached-center.shepherd-target-attached-top.shepherd-target-attached-center.shepherd-enabled > div > footer > ul > li:nth-child(2) > a')
 
 
+    await page.waitFor(4000) // not sure why we have this.
+
+    await page.click(`.shepherd-button-example-primary`)
+
+    await page.waitFor(4000) // not sure why we have this.
+
+    await page.click(`#tune-relevance-link > a:nth-child(2)`)
 
 
     await browser.close()
