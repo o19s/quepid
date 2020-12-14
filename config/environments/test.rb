e792eec5b025df96adeb26c926434443c46ba895
epugh@opensourceconnections.com <>
2020-12-14 07:41:48 -0500

    Update test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: "localhost", port: '3000' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: '3000' }
 
   ENV['QUEPID_GA'] = 'UA-FAKE-GA-CODE-FOR-TESTING'
 end
