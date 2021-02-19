12641801b0d06a2422c6d485c8a53ced14add44b
Gabriel Bauer <gabriel.bauer@commerce-experts.com>
2021-02-19 15:23:19 +0100

    Fixed linting failures

--- config/initializers/smtp_mail_provider.rb
+++ config/initializers/smtp_mail_provider.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 # Make mail sending possible over smtp and configurable via env vars.
-if ENV['MAIL_METHOD'].present? && ENV['MAIL_METHOD'] === "smtp"
+if ENV['MAIL_METHOD'].present? && 'smtp'.casecmp(ENV['MAIL_METHOD']).zero?
   Rails.application.config.action_mailer.delivery_method = :smtp
   Rails.application.config.action_mailer.smtp_settings = {
     address:              ENV['SMTP_HOST'],
@@ -8,6 +10,6 @@ if ENV['MAIL_METHOD'].present? && ENV['MAIL_METHOD'] === "smtp"
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       ENV['SMTP_AUTHENTICATION_TYPE'],
-    enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS']
+    enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS'],
   }
 end
