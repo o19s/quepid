83dc235bb4df0aa015baa295dabc77ebfc2df795
epugh@opensourceconnections.com <>
2021-03-09 16:36:47 -0500

    test if we need this.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,9 +5,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-#Rails.application.config.assets.configure do |env|
+# Maybe needed with segfaults?
+# Rails.application.config.assets.configure do |env|
 #  env.export_concurrent = false
-#end
+# end
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
