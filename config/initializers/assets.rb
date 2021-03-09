957560d9f5b8519f5489a0786d764bb96e11ac96
epugh@opensourceconnections.com <>
2021-03-09 15:29:23 -0500

    move code

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
+end
+
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
