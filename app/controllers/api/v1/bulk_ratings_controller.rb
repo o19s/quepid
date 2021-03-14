bcef7002b16b757f7dd5b97afb822df560e17f3f
epugh@opensourceconnections.com <>
2021-03-14 09:57:37 -0400

    we had a spurious "not permitted" parameters because we were using strong paramters and ratings_params
    now lets just pluck out the ONE field we need, which will prevent random params goign int....

--- app/controllers/api/v1/bulk_ratings_controller.rb
+++ app/controllers/api/v1/bulk_ratings_controller.rb
@@ -8,11 +8,17 @@ module Api
       before_action :set_case_query
       before_action :check_query
 
+      # We get a messy set of params in this method, so we don't use the normal
+      # approach of strong parameter validation.  We hardcode the only params
+      # we care about.
       def update
         if params[:doc_ids].present?
           params[:doc_ids].each do |doc_id|
             rating = @query.ratings.find_or_create_by doc_id: doc_id
-            rating.update rating_params
+
+            #rating.update rating_params
+            rating.rating = pluck_out_just_rating_param
+            rating.save
           end
         end
 
@@ -34,8 +40,14 @@ module Api
 
       private
 
-      def rating_params
-        params.permit(:rating)
+      # def rating_params
+      #  params.permit(:rating)
+      # should be params.require(:rating).permit(:rating) to
+      # follow standard pattern.
+      # end
+
+      def pluck_out_just_rating_param
+        params[:rating]
       end
     end
   end
