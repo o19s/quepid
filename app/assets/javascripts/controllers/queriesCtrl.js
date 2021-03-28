2c13888591d2d7be55ef960d77ebfdfaa41bcf2b
Daniel Worley <danworley@gmail.com>
2021-03-28 10:15:22 -0400

    Fix the annotations/lastScore wonkiness

--- app/assets/javascripts/controllers/queriesCtrl.js
+++ app/assets/javascripts/controllers/queriesCtrl.js
@@ -238,10 +238,10 @@ angular.module('QuepidApp')
               tryNo = acase.lastTry;
 
               lastScoreTracker = {
-                'score':      $scope.queries.avgQuery.lastScore.score,
-                'all_rated':  $scope.queries.avgQuery.lastScore.allRated,
+                'score':      $scope.queries.avgQuery.currentScore.score,
+                'all_rated':  $scope.queries.avgQuery.currentScore.allRated,
                 'try_number': tryNo,
-                'queries':    $scope.queries.avgQuery.lastScore.queries,
+                'queries':    $scope.queries.avgQuery.currentScore.queries,
               };
 
               $log.info('sending score information to mothership');
@@ -250,10 +250,10 @@ angular.module('QuepidApp')
         }
         else {
           var lastScoreTracker = {
-            'score':      $scope.queries.avgQuery.lastScore.score,
-            'all_rated':  $scope.queries.avgQuery.lastScore.allRated,
+            'score':      $scope.queries.avgQuery.currentScore.score,
+            'all_rated':  $scope.queries.avgQuery.currentScore.allRated,
             'try_number': tryNo,
-            'queries':    $scope.queries.avgQuery.lastScore.queries,
+            'queries':    $scope.queries.avgQuery.currentScore.queries,
           };
 
           $log.info('sending score information to mothership');
