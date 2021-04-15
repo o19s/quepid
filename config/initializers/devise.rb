8b11cd3d9eb9e602e083ead7f4293ebc7246b88a
Sławomir Maciąg <slawomir.maciag@rubix.com>
2021-04-15 17:23:25 +0200

    Email sender from env variable, small prod fixes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'quepid@o19s.com'
+  config.mailer_sender = ENV['EMAIL_SENDER']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
