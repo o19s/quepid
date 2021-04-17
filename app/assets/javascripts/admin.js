4948a60951bc2f38f624f91e8a7a33d9d5a2f6c8
epugh@opensourceconnections.com <>
2021-04-17 09:32:04 -0400

    fix up admin deleting of users.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,9 +1,14 @@
 //= require jquery
+//= require rails-ujs
+
 //= require bootstrap/dist/js/bootstrap.bundle
+
 //= require d3
 //= require cal-heatmap
+
 //= require ace-rails-ap
 //= require ace/theme-chrome
 //= require ace/mode-javascript
+
 //= require admin/scorers
 //= require admin/user_pulse
