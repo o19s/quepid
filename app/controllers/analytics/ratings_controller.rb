66a88318ef5e59a378da90aaa3481f3518153a39
epugh@opensourceconnections.com <>
2021-04-25 14:04:37 -0400

    checkpoint while I deal with uglifier issues

--- app/controllers/analytics/ratings_controller.rb
+++ app/controllers/analytics/ratings_controller.rb
@@ -10,9 +10,55 @@ module Analytics
     # GET /admin/users/1
     # GET /admin/users/1.json
     def show
-      @df = Rover::DataFrame.new(@case.ratings)
+      ratings = @case.ratings
+      users = []
+      ratings.each do |rating|
+        if rating.rating.nil?
+          rating.rating = -1
+        end
+        puts "rating user: #{rating.user_id}"
+        users << rating.user.id unless rating.user.nil?
+      end
+      @df = Rover::DataFrame.new(ratings)
       @df.delete('updated_at')
       @df.delete('created_at')
+
+      puts @df.keys
+
+
+      users.uniq!
+
+      puts "here are users"
+      puts users
+
+      df1 = Rover::DataFrame.new(@case.ratings.where('`ratings`.`user_id` = ?',users.first))
+      df2 = Rover::DataFrame.new(@case.ratings.where('`ratings`.`user_id` = ?',users.second))
+
+      df1.delete('updated_at')
+      df1.delete('created_at')
+      df1["user_#{users.first}"] = df1.delete("user_id")
+      df1["rating_#{users.first}"] = df1.delete("rating")
+
+      df2.delete('updated_at')
+      df2.delete('created_at')
+      df2["user_#{users.second}"] = df2.delete("user_id")
+      df2["rating_#{users.second}"] = df2.delete("rating")
+      puts df1.to_csv
+      puts df2.to_csv
+
+      df1 = df1.inner_join(df2, on: ["query_id", "doc_id"])
+
+      puts "JOINED"
+      puts df1.to_csv
+
+      puts "Average"
+      df1['average']  = (df1['rating_218954774'] * df1['rating_962534057']) / 2
+      puts df1.to_csv
+     #@df['rating'][@df['rating'] == nil] = -1
+
+      #puts @df['doc_id'].crosstab(@df['rating'])
+
+
 
       # @df.sort_by! { | r | r[:query_id]}
       # @user_keys = []
