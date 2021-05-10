342dd963611b1796903f74ecbce4dd5f24fcf673
epugh@opensourceconnections.com <>
2021-05-10 14:46:38 -0400

    I dont often code in master, but when I do, I always mess it up the first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_221952) do
+ActiveRecord::Schema.define(version: 2021_05_10_175459) do
 
   create_table "annotations", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "message"
