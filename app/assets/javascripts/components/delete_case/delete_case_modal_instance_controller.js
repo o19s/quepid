333b5b95a04589511bbc1058639f1e9218ac73ce
epugh@opensourceconnections.com <>
2021-05-19 07:25:28 -0400

    Rework archive function to be based off of permissions.  Oh, and lets get rid of this onlyCase idea.
    It's okay to have a team with no cases..  the 1 case required is an old concept from early Quepid days!   We have moved beyond it.

--- app/assets/javascripts/components/delete_case/delete_case_modal_instance_controller.js
+++ app/assets/javascripts/components/delete_case/delete_case_modal_instance_controller.js
@@ -2,12 +2,22 @@
 
 angular.module('QuepidApp')
   .controller('DeleteCaseModalInstanceCtrl', [
+    '$rootScope',
     '$uibModalInstance',
     'onlyCase',
-    function ($uibModalInstance, onlyCase) {
+    function ($rootScope, $uibModalInstance, onlyCase) {
       var ctrl = this;
 
       ctrl.onlyCase = onlyCase;
+
+      ctrl.canDelete = false;
+
+      $rootScope.$watch('currentUser', function() {
+        if ( $rootScope.currentUser ) {
+          console.log($rootScope.currentUser.permissions.case)
+          ctrl.canDelete = $rootScope.currentUser.permissions.case.delete;
+        }
+      });
 
       ctrl.ok = function () {
         $uibModalInstance.close(true);
