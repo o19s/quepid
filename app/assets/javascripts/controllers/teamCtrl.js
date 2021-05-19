c15df57694cb1eb80751de4904910579c39be2de
epugh@opensourceconnections.com <>
2021-05-19 12:35:13 -0400

    listen for updatedCasesList event in order to repaint the list

--- app/assets/javascripts/controllers/teamCtrl.js
+++ app/assets/javascripts/controllers/teamCtrl.js
@@ -8,28 +8,43 @@ angular.module('QuepidApp')
       $scope, $routeParams, $location,
       teamSvc
     ) {
+      var ctrl = this;
+      
       $scope.teamModel   = {};
       var teamId         = parseInt($routeParams.teamId, 10);
       $scope.currentTeam = {};
+
+      // Functions
+      ctrl.fetchTeam = fetchTeam;
 
       $scope.$on('teamUpdated', function(event, team) {
         $scope.currentTeam = team;
       });
 
       $scope.teamModel.show = function(teamId) {
-        teamSvc.get(teamId, true)
-          .then(function(response) {
-            $scope.currentTeam = response;
-          }, function() {
-            $location.path('/teams');
-          });
+        fetchTeam();
       };
+
+      angular.forEach(['updatedCasesList'], function (eventName) {
+        $scope.$on(eventName, function() {
+          fetchTeam();
+        });
+      });
 
       var init = function() {
         if (teamId) {
           $scope.teamModel.show(teamId);
         }
       };
+
+      function fetchTeam () {
+        teamSvc.get(teamId, true)
+          .then(function(response) {
+            $scope.currentTeam = response;
+          }, function() {
+            $location.path('/teams');
+          });
+      }
 
       init();
     }
