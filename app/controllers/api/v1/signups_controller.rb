0334d46145efe91eac5c719644f8965a6ffa335e
epugh@opensourceconnections.com <>
2021-05-21 15:49:54 -0400

    Follow up to PR #347 to make sure we pop the wizard properly!

--- app/controllers/api/v1/signups_controller.rb
+++ app/controllers/api/v1/signups_controller.rb
@@ -21,6 +21,9 @@ module Api
             @user.assign_attributes(user_params_to_save)
           else
             @user = User.new user_params_to_save
+            # in this flow, we have a new user joining, so we create a empty case for them, which
+            # on the home_controller.rb triggers the bootstrap and the new case wizard.
+            @user.cases.build case_name: "Case #{@user.cases.size}"
           end
         end
 
