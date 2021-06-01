19e6f74e9fc0d14f5883e741d0c55188cbcb1b9f
Eric Pugh <epugh@opensourceconnections.com>
2021-06-01 18:06:05 -0400

    Handle flows when no email delivery method (#369)
    * properly handle no email delivery service configured.
    
    * lint
    
    Co-authored-by: epugh@opensourceconnections.com <>

---
+++ app/controllers/concerns/notifications_manager.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module NotificationsManager
+  extend ActiveSupport::Concern
+
+  private
+
+  def email_notifications_enabled?
+    Rails.application.config.action_mailer.delivery_method.present?
+  end
+
+  def check_email
+    unless email_notifications_enabled?
+      redirect_to secure_path, alert: "Email delivery hasn't been set up, so can't send reset password email."
+    end
+  end
+end
