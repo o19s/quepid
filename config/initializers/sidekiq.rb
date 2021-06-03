0c2f3997343b2e8dce7ee6f028ccc9e435cdace4
epugh@opensourceconnections.com <>
2021-06-03 16:45:19 -0400

    rubocop

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,9 +2,8 @@
 
 Sidekiq::Extensions.enable_delay!
 
-sidekiq_config = { url: ENV['REDIS_URL'],
-                   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-                 }
+sidekiq_config = { url:        ENV['REDIS_URL'],
+                   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
