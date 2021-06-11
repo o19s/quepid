102c200a165500e457b76d6933d5070989f316e1
epugh@opensourceconnections.com <>
2021-06-11 18:17:38 -0400

    add in clone scorer component

---
+++ app/assets/javascripts/components/clone_scorer/clone_scorer_modal_instance_controller.js
@@ -0,0 +1,53 @@
+'use strict';
+
+angular.module('QuepidApp')
+  .controller('CloneScorerModalInstanceCtrl', [
+    '$scope',
+    '$uibModalInstance',
+    'scorerControllerActionsSvc',
+    'customScorerSvc',
+    'scorer',
+    function (
+      $scope,
+      $uibModalInstance,
+      scorerControllerActionsSvc,
+      customScorerSvc,
+      scorer
+    ) {
+      var ctrl = this;
+      ctrl.needToWarnOnScaleChange = false; // New scorers don't have any ratings, so no need to warn.
+
+      scorerControllerActionsSvc.addActions(ctrl, $scope);
+
+      ctrl.scorer       = angular.copy(scorer);
+      ctrl.scorer.name = 'Clone of ' + ctrl.scorer.name;
+
+      if (customScorerSvc.scalesAreEqual(
+        ctrl.scorer.scale, ctrl.scaleOptions.detailScale
+      )) {
+        ctrl.scaleChoice = 'detailScale';
+      } else if (customScorerSvc.scalesAreEqual(
+        ctrl.scorer.scale, ctrl.scaleOptions.gradedScale
+      )) {
+        ctrl.scaleChoice = 'gradedScale';
+      } else if (customScorerSvc.scalesAreEqual(
+        ctrl.scorer.scale, ctrl.scaleOptions.binaryScale
+      )) {
+        ctrl.scaleChoice = 'binaryScale';
+      } else  {
+        ctrl.scaleChoice = 'custom';
+      }
+
+      ctrl.scorerOptions = {
+        showName: true
+      };
+
+      ctrl.ok = function () {
+        $uibModalInstance.close(ctrl.scorer);
+      };
+
+      ctrl.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+      };
+    }
+  ]);
