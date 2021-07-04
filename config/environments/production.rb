760680daaf7d152a4b029b561f063bd3281820ad
epugh@opensourceconnections.com <>
2021-07-04 07:08:41 -0400

    needed this setting to turn it on

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: ENV['QUEPID_DOMAIN'] }
 
-  #config.force_ssl = true if 'true' == ENV['FORCE_SSL']
+  config.force_ssl = true if 'true' == ENV['FORCE_SSL']
   puts "HERE WE GO, WE ARE SETTING UP SSL_OPTIONS\n\n"
   config.ssl_options = { hsts: false, redirect: { exclude: ->(request) { request.path =~ /profile/ } } }
   #config.ssl_options = { redirect: { constrain_to: -> request { request.path != '/' } } }
