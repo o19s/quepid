a5639c8e0fff1c8436843247be34715d7a4725aa
epugh@opensourceconnections.com <>
2021-07-04 16:41:28 -0400

    work aorund

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [ :get, :post, :patch, :put ], credentials: true
+    origins '/(.*)/'
+    resource '*', headers: :any, methods: [ :get, :post, :patch, :put ], credentials: false
   end
 end
 
