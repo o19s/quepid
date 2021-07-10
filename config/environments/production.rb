1e5bc5fa7ad2c0f63f610dd2224657a154c91f3e
epugh@opensourceconnections.com <>
2021-07-10 16:40:31 -0400

    maybe?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['QUEPID_DOMAIN'] }
 
   config.force_ssl = true if 'true' == ENV['FORCE_SSL']
-  config.ssl_options = { secure_cookies: false, hsts: false, redirect: { exclude: -> request { request.path =~ /api/ or request.path =~ /assets/ or request.path == '/' } } } 
+  config.ssl_options = { secure_cookies: false, hsts: false, redirect: { exclude: -> request { request.path =~ /api/ or request.path =~ /assets/ or request.path =~ /case/ or request.path == '/' } } } 
 end
 
 # rubocop:enable Metrics/BlockLength
