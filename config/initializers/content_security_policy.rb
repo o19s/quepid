20a58e575071c8e11a6dcccc0bf649607af3fb82
epugh@opensourceconnections.com <>
2021-07-10 16:33:39 -0400

    maybe/

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,12 +5,12 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
  Rails.application.config.content_security_policy do |policy|
-   policy.default_src :self, :https
+   #policy.default_src :self, :https
    policy.font_src    :self, :https, :data
-   policy.img_src     :self, :https, :data
-   policy.object_src  :none
-   policy.script_src  :self, :https
-   policy.style_src   :self, :https
+   #policy.img_src     :self, :https, :data
+   #policy.object_src  :none
+   #policy.script_src  :self, :https
+   #policy.style_src   :self, :https
 
    # Specify URI for violation reports
     policy.report_uri "/csp-violation-report-endpoint"
