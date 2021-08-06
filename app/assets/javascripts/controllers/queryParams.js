629ee576eedb034c94cddfb293b1caca4d530e4c
epugh@opensourceconnections.com <>
2021-08-06 17:18:12 -0400

    fixes #390 by warning on solr syntax issues

--- app/assets/javascripts/controllers/queryParams.js
+++ app/assets/javascripts/controllers/queryParams.js
@@ -8,6 +8,37 @@ angular.module('QuepidApp')
       $scope.qp = {};
       $scope.qp.curTab = 'developer';
 
+      $scope.showQueryParamsWarning = false;
+      $scope.queryParamsWarning = '';
+
+      $scope.validateQueryParams = function () {
+        console.log("LETS VALIDATE");
+        console.log($scope.settings.selectedTry.queryParams);
+
+        var params = {
+          deftype:               'defType',
+          echoparams:            'echoParams',
+          explainother:          'explainOther',
+          logparamslist:         'logParamsList',
+          omitheader:            'omitHeader',
+          segmentterminateearly: 'segmentTerminateEarly',
+          timeallowed:           'timeAllowed',
+        };
+
+        $scope.showQueryParamsWarning = false;
+
+        for (var key in params) {
+          var correct = params[key];
+          var re      = new RegExp(key);
+
+          if ( $scope.settings.selectedTry.queryParams.match(re) ) {
+            $scope.queryParamsWarning = 'Your query params contain <code>' + key + '</code>, you probably meant <code>' + correct + '</code>.';
+            $scope.showQueryParamsWarning = true;
+            break;
+          }
+        }
+      };
+
       $scope.qp.toggleTab = function(a) {
         $scope.qp.curTab = a;
 
