85037c109faff56c7030bddbd45fb4821552c14a
epugh@opensourceconnections.com <>
2021-09-27 06:55:14 -0400

    argh furling

--- app/controllers/concerns/authentication/current_user_manager.rb
+++ app/controllers/concerns/authentication/current_user_manager.rb
@@ -47,10 +47,12 @@ module Authentication
         # check if we are redirected from the case page, and if so lets support unfurling
         # by populating the flash so it renders in the start.html.erb layout.
         if params[:controller] == 'home' && params[:action] == 'index' && params[:id]
-          puts "Setting up unfurl for case #{params[:id]}"
-          flash[:unfurl] = Case.find_by_id(params[:id])
+          #puts "Setting up unfurl for case #{params[:id]}"
+          #flash[:unfurl] = Case.find_by_id(params[:id])
+          redirect_to new_session_path
+        else
+          redirect_to new_session_path, @case_id = params[:id]
         end
-        redirect_to new_session_path
       end
     end
 
