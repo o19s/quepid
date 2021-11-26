9344677196778ef36bc170d98411ffcbbae2678d
epugh@opensourceconnections.com <>
2021-11-26 14:03:53 -0500

    starting to move to bootstrap 5 for portions of the app that cna

--- app/assets/javascripts/account.js
+++ app/assets/javascripts/account.js
@@ -11,13 +11,10 @@
 // about supported directives.
 //
 // This file loads all the below JavaScript files and can be very slow in the dev mode.
-//
-// Use the bootstrap-sprockets instead of bootstrap when you need individual boostrap files for
-// debugging.  Otherwise the bootstrap version loads all the files as a single minified file.
 
 //= require jquery
 //= require rails-ujs
 
-//= require cookies_eu
-//= require bootstrap/dist/js/bootstrap.bundle
+// Commented out
+//require bootstrap/dist/js/bootstrap.bundle
 //= require turbolinks
