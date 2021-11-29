3f41c62d66be9fbc24324acc1d8cf7cf1a447f2e
epugh@opensourceconnections.com <>
2021-11-29 17:46:15 -0500

    now have the footer sticking to the bottom, and have admin and account looking similar

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,15 +1,15 @@
-//= require jquery
-//= require rails-ujs
+// require jquery
+// require rails-ujs
 
 //= require bootstrap/dist/js/bootstrap.bundle
 
-//= require d3
-//= require cal-heatmap
+// require d3
+// require cal-heatmap
 
-//= require ace-builds/src-min-noconflict/ace
-//= require ace-builds/src-min-noconflict/ext-language_tools
-//= require ace-builds/src-min-noconflict/mode-json
-//= require ace-builds/src-min-noconflict/mode-javascript
+// require ace-builds/src-min-noconflict/ace
+// require ace-builds/src-min-noconflict/ext-language_tools
+// require ace-builds/src-min-noconflict/mode-json
+// require ace-builds/src-min-noconflict/mode-javascript
 
-//= require admin/scorers
-//= require admin/user_pulse
+// require admin/scorers
+// require admin/user_pulse
