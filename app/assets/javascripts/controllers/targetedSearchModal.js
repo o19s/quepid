031e852884e5d436f19b7b83a425bd0751469d55
epugh@opensourceconnections.com <>
2021-12-16 21:04:58 -0500

    lets format our Lucene query!

--- app/assets/javascripts/controllers/targetedSearchModal.js
+++ app/assets/javascripts/controllers/targetedSearchModal.js
@@ -8,10 +8,22 @@ angular.module('QuepidApp')
 
       $scope.query = query;
       $scope.targetedSearchModalModel = {};
+      $scope.enterKeyPressed = false;
+
       $log.debug('TargetedSearchModalCtrl - Initiated');
 
       $scope.targetedSearchModalModel.closeModal = function() {
         $uibModalInstance.dismiss('cancel');
+      };
+
+      $scope.checkIfEnterKeyPressed = function(event){
+        console.log (event.keyCode)
+        if (event.keyCode === 13) {
+          $scope.enterKeyPressed = true;
+        }
+        else {
+          $scope.enterKeyPressed = false;
+        }
       };
     }
   ]);
