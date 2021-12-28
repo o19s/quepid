e533bde8500a9ab428c1dccebe3951d9d262c655
epugh@opensourceconnections.com <>
2021-12-28 09:31:07 -0500

    fix up

--- app/assets/javascripts/controllers/queryParams.js
+++ app/assets/javascripts/controllers/queryParams.js
@@ -30,7 +30,7 @@ angular.module('QuepidApp')
         console.log('searchUrl:' + $scope.settings.searchUrl);
 
         console.log('searchUrl:' + $scope.settings.searchUrl != '');
-        console.log('angular.isUndefined:'' + angular.isUndefined($scope.settings.searchUrl));
+        console.log('angular.isUndefined:' + angular.isUndefined($scope.settings.searchUrl));
         if ($scope.settings.searchEngine != ''){
           // Figure out if we need to redirect based on our search engine's url.
           var quepidStartsWithHttps = $location.protocol() === 'https';
