931e3b3b84195dff028884195e467021dea6e6af
epugh@opensourceconnections.com <>
2022-03-15 16:57:01 -0400

    move towards Bootstrap 5 in the forms...

--- app/assets/javascripts/start.js
+++ app/assets/javascripts/start.js
@@ -10,4 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap/dist/js/bootstrap.bundle
 //= require jquery
