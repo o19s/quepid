c963f13f93afccee402653227333a34241df859b
Eric Pugh <epugh@opensourceconnections.com>
2022-04-07 10:50:36 -0400

    Ancestry column too short (#479)
    * test now passes
    
    * it's working!
    
    * fix #470
    
    * reset the tries ancestry tree if we get to length too long
    
    * simplify logic and check case.save
    
    * rubocopy
    
    Co-authored-by: epugh@opensourceconnections.com <>

--- app/controllers/api/v1/tries_controller.rb
+++ app/controllers/api/v1/tries_controller.rb
@@ -11,6 +11,8 @@ module Api
         @tries = @case.tries
       end
 
+      # rubocop:disable Metrics/MethodLength
+      # rubocop:disable Metrics/AbcSize
       def create
         parameters_to_use = try_params
         if params[:parent_try_number] # We need special translation from try_number to the try.id
@@ -23,7 +25,15 @@ module Api
         @try.try_number       = try_number
         @case.last_try_number = try_number
 
-        if @try.save && @case.save
+        # be smart about ancestry tracking leading too long of a string for database column.
+        begin
+          case_saved = @case.save
+        rescue ActiveRecord::ValueTooLong
+          @try.parent = nil # restart the ancestry tracking!
+          case_saved = @case.save
+        end
+
+        if case_saved
           @try.add_curator_vars params[:curatorVars]
           Analytics::Tracker.track_try_saved_event current_user, @try
 
@@ -32,6 +42,8 @@ module Api
           render json: @try.errors.concat(@case.errors), status: :bad_request
         end
       end
+      # rubocop:enable Metrics/MethodLength
+      # rubocop:enable Metrics/AbcSize
 
       def show
         respond_with @try
---
+++ test/integration/tries_ancestry_lifecycle_test.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class TriesAncestryLifecycleTest < ActionDispatch::IntegrationTest
+  test 'create 200 tries behavior' do
+    root = Try.new(name: 'Try Root')
+    root.save!
+
+    parent = root
+    200.times do |i|
+      try = Try.new(name: "Try #{i}")
+      try.parent = parent
+      try.save!
+      parent = try
+    end
+  end
+
+  test 'Creates a new root try when ancestry tree gets to be too much' do
+    root_tries = []
+    root = Try.new(name: 'Try Root')
+    root.save!
+    root_tries << root
+
+    value_too_long_condition_hit = false
+    parent = root
+    300.times do |i|
+      try = Try.new(name: "Try #{i}")
+      try.parent = parent
+      begin
+        try.save!
+      rescue ActiveRecord::ValueTooLong
+        value_too_long_condition_hit = true
+        try.parent = nil
+        try.save!
+        root_tries << try
+      end
+      parent = try
+    end
+
+    assert value_too_long_condition_hit
+    assert_equal 2, root_tries.length
+  end
+end
