f6de514ff6d8b463a82f22cc44de28ca6e41672e
Daniel Worley <danworley@gmail.com>
2022-04-26 10:13:58 -0400

    Prep config svc changes

--- app/assets/javascripts/services/configurationSvc.js
+++ app/assets/javascripts/services/configurationSvc.js
@@ -5,7 +5,11 @@ angular.module('UtilitiesModule')
     function ConfigurationSvc() {
       var communalScorersOnly;
       var queryListSortable;
+      var prefixPath;
 
+      this.getApiPath = function() {
+        return prefixPath() + "api/";
+      }
 
       this.setCommunalScorersOnly = function(val) {
         communalScorersOnly = JSON.parse(val);
@@ -14,6 +18,15 @@ angular.module('UtilitiesModule')
       this.isCommunalScorersOnly = function() {
         return communalScorersOnly;
       };
+
+      this.setPrefixPath = function(val) {
+        prefixPath = val;
+      };
+
+      this.prefixPath = function() {
+        // Rails doesn't work if path ends with slash, angular requires it
+        return prefixPath ? (prefixPath + "/") : "/";
+      }
 
       this.setQueryListSortable = function (val) {
         queryListSortable = JSON.parse(val);
