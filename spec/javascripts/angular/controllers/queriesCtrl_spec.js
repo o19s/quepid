9e85692265334e025f8b561876f9f2a9cbf6c922
epugh@opensourceconnections.com <>
2022-05-16 13:22:29 -0400

    more robust filtering

---
+++ spec/javascripts/angular/controllers/queriesCtrl_spec.js
@@ -0,0 +1,60 @@
+'use strict';
+
+describe('Controller: QueriesCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('QuepidTest'));
+
+  var QueriesCtrl,
+    scope;
+
+  var starWarsQuery = {queryId: 1, queryText: 'star wars'};
+  var starTrekQuery = {queryId: 2, queryText: 'star trek'};
+  var starManQuery = {queryId: 3, queryText: 'STARMAN'};
+  var boxingQuery = {queryId: 4, queryText: 'The Boxing Match'};
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    QueriesCtrl = $controller('QueriesCtrl', {
+      $scope: scope
+    });
+  }));
+
+
+  it('no query filter defined matches everything', function () {
+    expect(scope.matchQueryFilter(starWarsQuery)).toBe(true);
+  });
+
+  it('use query filter to match queries', function () {
+
+    scope.queryFilter = 'star';
+
+    expect(scope.matchQueryFilter(starWarsQuery)).toBe(true);
+    expect(scope.matchQueryFilter(starTrekQuery)).toBe(true);
+
+    scope.queryFilter = 'boxing';
+    expect(scope.matchQueryFilter(starTrekQuery)).toBe(false);
+  });
+
+  it('use query filter to match mixed case queries and filter', function () {
+
+    scope.queryFilter = 'Star';
+
+    expect(scope.matchQueryFilter(starWarsQuery)).toBe(true);
+    expect(scope.matchQueryFilter(starTrekQuery)).toBe(true);
+    expect(scope.matchQueryFilter(starManQuery)).toBe(true);
+    expect(scope.matchQueryFilter(boxingQuery)).toBe(false);
+
+  });
+
+  it('use query filter to match mixed case queries', function () {
+
+    scope.queryFilter = 'war';
+
+    expect(scope.matchQueryFilter(starWarsQuery)).toBe(true);
+    expect(scope.matchQueryFilter(starTrekQuery)).toBe(false);
+
+  });
+
+});
