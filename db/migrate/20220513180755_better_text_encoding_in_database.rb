70184a9d4028d5c45ba87672d850ea4e6acbc9ba
epugh@opensourceconnections.com <>
2022-05-16 09:22:10 -0400

    now using utf for the snapshot explain syntax

---
+++ db/migrate/20220513180755_better_text_encoding_in_database.rb
@@ -0,0 +1,12 @@
+class BetterTextEncodingInDatabase < ActiveRecord::Migration[6.1]
+  def change
+    # change all text encodings to default utf8mb4.
+    # did NOT change users.password as that is super old from the Flask app days!
+
+    BetterTextEncodingInDatabase.connection.execute(
+      "
+      ALTER TABLE snapshot_docs CHANGE `explain` `explain` MEDIUMTEXT  CHARACTER SET `utf8mb4`  COLLATE `utf8mb4_general_ci`  NULL;
+      "
+    )
+  end
+end
