204c82c7be86d90c06feb10ecc252383a626faac
epugh@opensourceconnections.com <>
2022-05-19 11:08:55 -0400

    rubocop

--- app/channels/rating_channel.rb
+++ app/channels/rating_channel.rb
@@ -1,40 +1,42 @@
+# frozen_string_literal: true
+
 class RatingChannel < ApplicationCable::Channel
   def subscribed
 
+    # rubocop:disable Layout/LineLength
     # this is fun.  From straight up JS: {channel: 'RatingChannel', case:7} is {"channel"=>"RatingChannel", "case"=>7}
     # but in Angular ActionCable: {channel: 'RatingChannel', case:7} is {"channel"=>"RatingChannel", "data"=>{"case"=>7}}
-    puts "in sbuscribed."
+    # rubocop:enable Layout/LineLength
+    puts 'in sbuscribed.'
     puts params
     case_id = params['data'].present? ? params['data']['case_id'] : params['case_id']
     puts "our case id is #{case_id}"
 
-
     stream_from "case-#{case_id}"
-    #stream_for current_user
+    # stream_for current_user
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def self.rating_created_event (current_case, current_user, rating)
+  def self.rating_created_event current_case, current_user, rating
     ActionCable.server.broadcast "case-#{current_case.id}", {
-      user: {
+      user:     {
         name: current_user.name,
-        id: current_user.id
+        id:   current_user.id,
       },
-      query_id: rating.query.id
+      query_id: rating.query.id,
     }
   end
 
-  def self.rating_bulk_updated_event (current_case, current_user, query)
+  def self.rating_bulk_updated_event current_case, current_user, query
     ActionCable.server.broadcast "case-#{current_case.id}", {
-      user: {
+      user:     {
         name: current_user.name,
-        id: current_user.id
+        id:   current_user.id,
       },
-      query_id: query.id
+      query_id: query.id,
     }
   end
-
 end
