117ff02e8fdc0f290dcf2ab9a05293f9a41c58a4
David Fisher <dfisher@opensourceconnections.com>
2022-05-25 08:02:30 -0400

    update to compute AP as trec_eval does.

--- db/scorers/ap@10.js
+++ db/scorers/ap@10.js
@@ -3,10 +3,23 @@ total = 0;
 
 // if less than K results, need to reduce K now or final score is too low
 k = numReturned() < k ? numReturned() : k
-
+// for each returned document, calculate precision each time a new
+// relevant document is added to the ranked list.
+var count = 0;
 eachDoc(function(doc, i) {
-    total += avgRating(i+1)
+    if (hasDocRating(i) && (docRating(i)) > 0) {
+        count++;
+    total += count/(i+1)
+    }
 }, k);
-
-var score = total / k;
+// count up the total number of relevant (not judged) documents
+var totalRel = 0;
+for (var i = 0; i < bestDocs.length; i++) {
+    if (bestDocs[i].rating > 0) {
+        totalRel++;
+    }
+}
+// AP is the sum of the precision points divided by the total
+// number of relevant documents
+var score = total / totalRel;
 setScore(score);
