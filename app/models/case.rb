b60bf099ab4075740df597bd896563b6d396790e
epugh@opensourceconnections.com <>
2022-05-26 08:22:31 -0400

    turns out we are missing some fields from our clone

--- app/models/case.rb
+++ app/models/case.rb
@@ -198,13 +198,15 @@ class Case < ApplicationRecord
   # rubocop:disable Metrics/MethodLength
   def clone_query query, clone_ratings
     new_query = ::Query.new(
-      arranged_next:  query.arranged_next,
-      arranged_at:    query.arranged_at,
-      query_text:     query.query_text,
-      notes:          query.notes,
-      threshold:      query.threshold,
-      threshold_enbl: query.threshold_enbl,
-      case:           self
+      arranged_next:    query.arranged_next,
+      arranged_at:      query.arranged_at,
+      query_text:       query.query_text,
+      notes:            query.notes,
+      threshold:        query.threshold,
+      threshold_enbl:   query.threshold_enbl,
+      options:          query.options,
+      information_need: query.information_need,
+      case:             self
     )
 
     if clone_ratings
