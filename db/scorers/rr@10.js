456553ab9b76011a0b743ebade1f3f067fc5a30e
David Fisher <dfisher@opensourceconnections.com>
2022-06-03 15:37:18 -0400

    #513 Add MRR to quepid as a communal scorer.

---
+++ db/scorers/rr@10.js
@@ -0,0 +1,9 @@
+let k = 10; // @Rank
+let rank = 0;
+eachDoc(function(doc, i) {
+    if (rank === 0 && hasDocRating(i) && (docRating(i)) > 0) {
+        rank = i+1; // remember the rank of the first relevant document
+     }
+}, k);
+const score = rank > 0 ? 1.0 / rank : 0.0;
+setScore(score);
