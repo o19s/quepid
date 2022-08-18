9cdc2e91138fc23b215f456cce95274637dfbcdd
epugh@opensourceconnections.com <epugh@opensourceconnections.com>
2022-08-18 13:56:50 -0400

    fix #1118

--- app/views/api/v1/scorers/_scorer.json.jbuilder
+++ app/views/api/v1/scorers/_scorer.json.jbuilder
@@ -2,19 +2,19 @@
 
 teams = scorer.teams.find_all { |t| current_user.teams.all.include?(t) }
 
-json.scorerId             scorer.id
-json.communal             scorer.communal
-json.code                 scorer.code
-json.name                 scorer.name
-json.scale                scorer.scale
-json.owner_id             scorer.owner_id
-json.owned                scorer.owner_id == current_user.id
-json.owner_name           scorer.owner.name unless scorer.owner.nil?
-json.manualMaxScore       scorer.manual_max_score
-json.manualMaxScoreValue  scorer.manual_max_score_value
-json.showScaleLabels      scorer.show_scale_labels
-json.scaleWithLabels      scorer.scale_with_labels
-json.teams                teams
+json.scorer_id                scorer.id
+json.communal                 scorer.communal
+json.code                     scorer.code
+json.name                     scorer.name
+json.scale                    scorer.scale
+json.owner_id                 scorer.owner_id
+json.owned                    scorer.owner_id == current_user.id
+json.owner_name               scorer.owner.name unless scorer.owner.nil?
+json.manual_max_score         scorer.manual_max_score
+json.manual_max_score_value   scorer.manual_max_score_value
+json.show_scale_labels        scorer.show_scale_labels
+json.scale_with_labels        scorer.scale_with_labels
+json.teams                    teams
 
 json.teams teams do |team|
   json.id         team.id
--- spec/javascripts/angular/services/scorerSvc_spec.js
+++ spec/javascripts/angular/services/scorerSvc_spec.js
@@ -8,7 +8,7 @@ describe('Service: scorerSvc', function () {
   var mockScorers = {
     'user_scorers': [
       {
-        'scorerId': 1,
+        'scorer_id': 1,
         'name':     'Scorer 1',
         'code':     'pass()',
         'owner_id': 1,
@@ -16,7 +16,7 @@ describe('Service: scorerSvc', function () {
         'scale':    [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10],
       },
       {
-        'scorerId': 2,
+        'scorer_id': 2,
         'name':     'Scorer 2',
         'code':     'pass()',
         'owner_id': 1,
@@ -24,7 +24,7 @@ describe('Service: scorerSvc', function () {
         'scale':    [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10],
       },
       {
-        'scorerId': 3,
+        'scorer_id': 3,
         'name':     'Scorer 3',
         'code':     'pass()',
         'owner_id': 2,
@@ -35,7 +35,16 @@ describe('Service: scorerSvc', function () {
   };
 
   var mockScorer = {
-    'scorerId': 1,
+    'scorerId':   1,
+    'name':       'Scorer 1',
+    'code':       'pass()',
+    'owner_id':   1,
+    'scale':      [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10],
+    'queryId':    null,
+  };
+
+  var mockScorerResp = {
+    'scorer_id':   1,
     'name':       'Scorer 1',
     'code':       'pass()',
     'owner_id':   1,
@@ -65,12 +74,12 @@ describe('Service: scorerSvc', function () {
 
   it('creates a scorer', function() {
     var url = '/api/scorers';
-    $httpBackend.expectPOST(url).respond(201, mockScorer);
+    $httpBackend.expectPOST(url).respond(201, mockScorerResp);
 
     scorerSvc.create(mockScorer)
       .then(function(response) {
         expect(scorerSvc.scorers.length).toBe(1);
-        var expectedScorer = scorerSvc.constructFromData(mockScorer);
+        var expectedScorer = scorerSvc.constructFromData(mockScorerResp);
         expect(angular.equals(response, expectedScorer)).toBe(true);
       });
     $httpBackend.flush();
@@ -79,9 +88,10 @@ describe('Service: scorerSvc', function () {
   it('edits a scorer', function() {
     var editedScorer  = mockScorer;
     editedScorer.name = 'Edited Scorer';
-
+    var mockEditedResp = mockScorerResp
+    mockEditedResp.name = 'Edited Scorer'
     var url = '/api/scorers/' + mockScorer.scorerId;
-    $httpBackend.expectPUT(url).respond(200, editedScorer);
+    $httpBackend.expectPUT(url).respond(200, mockEditedResp);
 
     scorerSvc.edit(editedScorer)
       .then(function(response) {
@@ -92,7 +102,7 @@ describe('Service: scorerSvc', function () {
 
   it('fetches a scorer', function() {
     var url = '/api/scorers/' + mockScorer.scorerId;
-    $httpBackend.expectGET(url).respond(200, mockScorer);
+    $httpBackend.expectGET(url).respond(200, mockScorerResp);
 
     scorerSvc.get(mockScorer.scorerId)
       .then(function(response) {
@@ -104,7 +114,9 @@ describe('Service: scorerSvc', function () {
   it('updates the cache when editing a scorer', function() {
     var url = '/api/scorers/' + mockScorer.scorerId;
     mockScorer.name = 'New Name';
-    $httpBackend.expectPUT(url).respond(200, mockScorer);
+    var mockEditedResp = mockScorerResp
+    mockEditedResp.name = 'New Name'
+    $httpBackend.expectPUT(url).respond(200, mockEditedResp);
 
     scorerSvc.edit(mockScorer).
       then(function() {
