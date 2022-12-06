1cb4c27ec36d2016842fec53272b96542763532c
Eric Pugh <epugh@opensourceconnections.com>
2022-12-06 12:01:48 -0500

    lint

--- config/initializers/uri.rb
+++ config/initializers/uri.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 # config/initializers/uri.rb
 
 # Ruby  3 got rid of this method, however the Gabba gem requires it.  Monkeypatch for now ;-(.
 
 module URI
-  def self.escape(*args)
+  def self.escape *args
     DEFAULT_PARSER.escape(*args)
   end
 end
