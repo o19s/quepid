969bcca624d5274101df8456a17e9f6557b14a15
David Fisher <dfisher@opensourcesolutions.com>
2023-01-17 13:23:21 -0500

    check for nil, migration will run, but there is no scorer in the DB when it runs.

--- db/migrate/20230117150814_fix_ap.rb
+++ db/migrate/20230117150814_fix_ap.rb
@@ -1,9 +1,13 @@
 class FixAp < ActiveRecord::Migration[6.1]
   def change
-    score_name = 'AP@10'
-    scorer = Scorer.where(name: scorer_name, communal: true).first
-    name = scorer.name
-    scorer.code = File.readlines("./db/scorers/#{name.downcase}.js",'\n').join('\n')
-    scorer.save!
+    scorers_to_update = ['AP@10']
+    scorers_to_update.each do |scorer_name|
+      scorer = Scorer.where(name: scorer_name, communal: true).first
+      unless scorer.nil?
+        name = scorer.name
+        scorer.code = File.readlines("./db/scorers/#{name.downcase}.js",'\n').join('\n')
+        scorer.save!
+      end
+    end
   end
 end
