95e25bb09a04c7f7afa5d9bced27dbb9cf7e9cf8
Eric Pugh <epugh@opensourceconnections.com>
2023-01-25 13:47:43 -0500

    bumping rails bumped sidekiq which bumped redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,6 @@
 # Redis and Sidekiq is used only for sending Google Analytics updates, which is
 # primarily used by the app.quepid.com install.  This checks if we need Sidekiq.
 if Rails.application.config.google_analytics_enabled
-  Sidekiq::Extensions.enable_delay!
 
   sidekiq_config = { url:        ENV.fetch('REDIS_URL', nil),
                      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
