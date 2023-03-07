860abd9f83adfde90336e40139010c81585e24ef
Eric Pugh <epugh@opensourceconnections.com>
2023-03-07 15:56:54 -0500

    got sloppy on migrations

--- db/migrate/20230307182739_rename_selection_strategy.rb
+++ db/migrate/20230307182739_rename_selection_strategy.rb
@@ -1,9 +1,8 @@
 class RenameSelectionStrategy < ActiveRecord::Migration[6.1]
   def change
-    selection_strategy = SelectionStrategy.where(name: 'TOTALLY_RANDOM').first_or_create(
-      name: 'Single Rater',
-      description: 'A single rating for each query/doc pair is all that is required.  The fastest way to get a lot of ratings, with lower quality.'
-    )
+    selection_strategy = SelectionStrategy.where(name: 'TOTALLY_RANDOM').first_or_create()
+    selection_strategy.name = 'Single Rater'
+    selection_strategy.description = 'A single rating for each query/doc pair is all that is required.  The fastest way to get a lot of ratings, with lower quality.'
     selection_strategy.save!
   end
 end
