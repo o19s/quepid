a324a7a19ddfb55f4818be435edec68af35863c3
Eric Pugh <epugh@opensourceconnections.com>
2023-05-09 16:56:52 -0400

    lint

--- app/assets/javascripts/controllers/queryParams.js
+++ app/assets/javascripts/controllers/queryParams.js
@@ -41,7 +41,7 @@ angular.module('QuepidApp')
               // Grab just the absolute url without any trailing query parameters
               var absUrl = $location.absUrl();
               var n = absUrl.indexOf('?');
-              $scope.quepidUrlToSwitchTo = absUrl.substring(0, n != -1 ? n : absUrl.length);
+              $scope.quepidUrlToSwitchTo = absUrl.substring(0, n !== -1 ? n : absUrl.length);
 
               $scope.quepidUrlToSwitchTo = $scope.quepidUrlToSwitchTo + '?searchEngine=' + $scope.settings.searchEngine + '&searchUrl=' + $scope.settings.searchUrl + '&showWizard=false&apiMethod=' + $scope.settings.apiMethod;
               $scope.quepidUrlToSwitchTo = $scope.quepidUrlToSwitchTo + '&fieldSpec=' + $scope.settings.fieldSpec;
--- app/assets/javascripts/controllers/wizardModal.js
+++ app/assets/javascripts/controllers/wizardModal.js
@@ -225,7 +225,7 @@ angular.module('QuepidApp')
           // Grab just the absolute url without any trailing query parameters
           var absUrl = $location.absUrl();
           var n = absUrl.indexOf('?');
-          $scope.quepidUrlToSwitchTo = absUrl.substring(0, n != -1 ? n : absUrl.length);
+          $scope.quepidUrlToSwitchTo = absUrl.substring(0, n !== -1 ? n : absUrl.length);
           
           $scope.quepidUrlToSwitchTo = $scope.quepidUrlToSwitchTo + '?searchEngine=' + $scope.pendingWizardSettings.searchEngine + '&searchUrl=' + $scope.pendingWizardSettings.searchUrl + '&showWizard=true&caseName=' + $scope.pendingWizardSettings.caseName + '&apiMethod=' + $scope.pendingWizardSettings.apiMethod;
 
