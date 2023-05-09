4b6b8029330b9bef739c4b7540e084fc753b1014
Eric Pugh <epugh@opensourceconnections.com>
2023-05-09 12:12:51 -0400

    appears that the magic redirecting on the server side was NOT helping us.

--- app/controllers/core_controller.rb
+++ app/controllers/core_controller.rb
@@ -3,8 +3,7 @@
 # This hosts the main Angular 1 application that runs in the client.
 class CoreController < ApplicationController
   before_action :set_case_or_bootstrap
-
-  #before_action :redirect_to_correct_tls # force a match to the URL of the search engine
+  before_action :populate_from_params
 
   def index
     # return unless current_user
@@ -30,22 +29,8 @@ class CoreController < ApplicationController
     end
   end
 
-  # If Quepid is running on HTTPS, like on Heroku, then it needs to switch
-  # to HTTP in order to make calls to a Solr that is running in HTTP as well, otherwise
-  # you get this "Mixed Content", which browsers block as a security issue.
-  # https://developer.mozilla.org/en-US/docs/Web/Security/Mixed_content
-  #
-  # Similarily we may have only HTTPS set up for Quepid, and therefore need to stay on HTTPS,
-  # so this method is only conditionally called if force_ssl is false.
-  #
-  # rubocop:disable Layout/LineLength
-  # rubocop:disable Metrics/MethodLength
-  # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/CyclomaticComplexity
-  # rubocop:disable Metrics/PerceivedComplexity
-  def redirect_to_correct_tls
-    return true unless Rails.application.config.redirect_to_match_search_engine_tls
-    return true if @case.blank? # shortcut if we don't have an @case.
+  
+  def populate_from_params
 
     if @case.present? && params[:caseName]
       @case.case_name = params[:caseName]
@@ -64,25 +49,6 @@ class CoreController < ApplicationController
       @try.save
     end
 
-    search_engine_starts_with_https = @try.present? && @try.search_url.present? ? @try.search_url.starts_with?('https') : false
-
-    if search_engine_starts_with_https && !request.ssl? # redirect to SSL
-      original_url = request.original_url
-      original_url.gsub!(%r{http://}, 'https://')
-      redirect_to original_url
-      false
-    elsif !search_engine_starts_with_https && request.ssl? # redirect to Non SSL
-      original_url = request.original_url
-      original_url.gsub!(%r{https://}, 'http://')
-      redirect_to original_url
-      false
-    else
-      true
-    end
+    true
   end
-  # rubocop:enable Layout/LineLength
-  # rubocop:enable Metrics/MethodLength
-  # rubocop:enable Metrics/AbcSize
-  # rubocop:enable Metrics/CyclomaticComplexity
-  # rubocop:enable Metrics/PerceivedComplexity
 end
