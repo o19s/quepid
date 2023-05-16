80b9a4da7f52306192b2b13ac70c09eb4f09ea3a
Christopher Morley <depahelix@gmail.com>
2023-05-16 14:32:55 -0400

    reformat code with tabs=2spaces

--- app/assets/javascripts/components/import_ratings/import_ratings_controller.js
+++ app/assets/javascripts/components/import_ratings/import_ratings_controller.js
@@ -10,48 +10,49 @@ angular.module('QuepidApp')
     'caseSvc',
     'queriesSvc',
     'querySnapshotSvc',
-    function($scope, $uibModal, flash, caseSvc, queriesSvc, querySnapshotSvc) {
+    function ($scope, $uibModal, flash, caseSvc, queriesSvc, querySnapshotSvc) {
       var ctrl = this;
 
       // Functions
       ctrl.create = create;
 
-      function create () {
+      function create() {
         var modalInstance = $uibModal.open({
-          templateUrl:  'import_ratings/_modal.html',
-          controller:   'ImportRatingsModalInstanceCtrl',
+          templateUrl: 'import_ratings/_modal.html',
+          controller: 'ImportRatingsModalInstanceCtrl',
           controllerAs: 'ctrl',
           size: 'lg',
-          resolve:      {
-            theCase: function() {
+          resolve: {
+            theCase: function () {
               return caseSvc.getSelectedCase(); //TODO not sure why this is needed for import snapshot to work?
             },
-            querySnapshotSvc: function() {
+            querySnapshotSvc: function () {
               return querySnapshotSvc;
             },
-            flash: function() {
+            flash: function () {
               return flash;
             },
-            queriesSvc: function() {
+            queriesSvc: function () {
               return flash;
             }
           }
         });
 
         modalInstance.result.then(
-          function(response) {
-            if ( !response.error ) {
+          function (response) {
+            if (!response.error) {
               queriesSvc.reset();
               queriesSvc.bootstrapQueries(ctrl.acase.caseNo)
-                  .then(function() {
-                    queriesSvc.searchAll();
-                  });
+                .then(function () {
+                  queriesSvc.searchAll();
+                });
 
               flash.success = response.message;
             } else {
               flash.error = response.message;
             }
-          }, function() { }
+          }, function () {
+          }
         );
       }
     }
--- app/assets/javascripts/components/import_ratings/import_ratings_modal_instance_controller.js
+++ app/assets/javascripts/components/import_ratings/import_ratings_modal_instance_controller.js
@@ -10,41 +10,41 @@ angular.module('QuepidApp')
     function ($scope, $uibModalInstance, importRatingsSvc, theCase, querySnapshotSvc) {
       var ctrl = this;
 
-      ctrl.theCase      = theCase;
-      ctrl.loading      = false;
+      ctrl.theCase = theCase;
+      ctrl.loading = false;
       ctrl.clearQueries = false;
-      ctrl.createQueries= false;
-      ctrl.csv          = {
-        content:          null,
-        header:           true,
-        separator:        ',',
+      ctrl.createQueries = false;
+      ctrl.csv = {
+        content: null,
+        header: true,
+        separator: ',',
         separatorVisible: false,
-        result:           null,
-        import:           {}
+        result: null,
+        import: {}
       };
       ctrl.information_needs = {
-        content:          null,
-        header:           true,
-        separator:        ',',
+        content: null,
+        header: true,
+        separator: ',',
         separatorVisible: false,
-        result:           null,
-        import:           {}
+        result: null,
+        import: {}
       };
       ctrl.snapshots = {
-        content:          null,
-        header:           true,
-        separator:        ',',
+        content: null,
+        header: true,
+        separator: ',',
         separatorVisible: false,
-        result:           null,
-        import:           {}
+        result: null,
+        import: {}
       };
 
-      ctrl.rre         = {
-        content:           null
+      ctrl.rre = {
+        content: null
       };
 
-      ctrl.ltr         = {
-        content:           null
+      ctrl.ltr = {
+        content: null
       };
 
       ctrl.options = {
@@ -52,41 +52,41 @@ angular.module('QuepidApp')
       };
 
       // Watches
-      $scope.$watch('ctrl.csv.content', function(newVal, oldVal) {
+      $scope.$watch('ctrl.csv.content', function (newVal, oldVal) {
         if (newVal !== oldVal) {
           ctrl.options.which = 'csv';
           ctrl.csv.import.alert = undefined;
           ctrl.checkCSVHeaders();
           ctrl.checkCSVBody();
         }
-      },true);
+      }, true);
 
-      $scope.$watch('ctrl.information_needs.content', function(newVal, oldVal) {
+      $scope.$watch('ctrl.information_needs.content', function (newVal, oldVal) {
         if (newVal !== oldVal) {
           ctrl.options.which = 'information_needs';
           ctrl.information_needs.import.alert = undefined;
           ctrl.checkInformationNeedsHeaders();
           ctrl.checkInformationNeedsBody();
         }
-      },true);
+      }, true);
 
-      $scope.$watch('ctrl.snapshots.content', function(newVal, oldVal) {
+      $scope.$watch('ctrl.snapshots.content', function (newVal, oldVal) {
         if (newVal !== oldVal) {
           ctrl.options.which = 'snapshots';
           ctrl.snapshots.import.alert = undefined;
           ctrl.checkSnapshotHeaders();
           ctrl.checkSnapshotBody();
         }
-      },true);
+      }, true);
 
-      $scope.pickedRREFile = function() {
+      $scope.pickedRREFile = function () {
         var f = document.getElementById('rreFile').files[0],
-            r = new FileReader();
+          r = new FileReader();
 
         // This next two lines don't seem to trigger the watches that I wanted.
         ctrl.options.which = 'rre';
         ctrl.loading = true;
-        r.onloadend = function(e) {
+        r.onloadend = function (e) {
           var data = e.target.result;
           ctrl.rre.content = data;
           ctrl.loading = false;
@@ -95,14 +95,14 @@ angular.module('QuepidApp')
         r.readAsText(f);
       };
 
-      $scope.pickedLTRFile = function() {
+      $scope.pickedLTRFile = function () {
         var f = document.getElementById('ltrFile').files[0],
-            r = new FileReader();
+          r = new FileReader();
 
         // This next two lines don't seem to trigger the watches that I wanted.
         ctrl.options.which = 'ltr';
         ctrl.loading = true;
-        r.onloadend = function(e) {
+        r.onloadend = function (e) {
           var data = e.target.result;
           ctrl.ltr.content = data;
           ctrl.loading = false;
@@ -121,27 +121,27 @@ angular.module('QuepidApp')
       };
 
       ctrl.ratingsTypePicked = function () {
-        return (ctrl.options.which === 'csv' || ctrl.options.which === 'rre' || ctrl.options.which === 'ltr' );
+        return (ctrl.options.which === 'csv' || ctrl.options.which === 'rre' || ctrl.options.which === 'ltr');
       };
 
       ctrl.informationNeedsTypePicked = function () {
-        return (ctrl.options.which === 'information_needs' );
+        return (ctrl.options.which === 'information_needs');
       };
 
       ctrl.snapshotsTypePicked = function () {
-        return (ctrl.options.which === 'snapshots' );
+        return (ctrl.options.which === 'snapshots');
       };
 
       ctrl.ok = function () {
-        if ( ctrl.options.which === 'snapshots' ) {
+        if (ctrl.options.which === 'snapshots') {
           var headers = ctrl.snapshots.content.split('\n')[0];
-          headers     = headers.split(ctrl.snapshots.separator);
+          headers = headers.split(ctrl.snapshots.separator);
 
           var expectedHeaders = [
             'Snapshot Name', 'Snapshot Time', 'Case ID', 'Query Text', 'Doc ID', 'Doc Position'
           ];
 
-          if ( !angular.equals(headers, expectedHeaders) ) {
+          if (!angular.equals(headers, expectedHeaders)) {
             var alert = 'Headers mismatch! Please make sure you have the correct headers in you file (check for correct spelling and capitalization): ';
             alert += '<br /><strong>';
             alert += expectedHeaders.join(',');
@@ -154,27 +154,27 @@ angular.module('QuepidApp')
           } else {
             ctrl.snapshots.import.loading = true;
             querySnapshotSvc.importSnapshotsToSpecificCase(ctrl.snapshots.result, theCase.caseNo)
-                .then(function() {
-                  var result = {
-                    success: true,
-                    message: 'Snapshots imported successfully!',
-                  };
-                  ctrl.snapshots.import.loading = false;
-                  $uibModalInstance.close(result);
-                }, function() {
-                  var result = {
-                    error:    true,
-                    message:  'Could not import snapshots successfully! Please try again.',
-                  };
+              .then(function () {
+                var result = {
+                  success: true,
+                  message: 'Snapshots imported successfully!',
+                };
+                ctrl.snapshots.import.loading = false;
+                $uibModalInstance.close(result);
+              }, function () {
+                var result = {
+                  error: true,
+                  message: 'Could not import snapshots successfully! Please try again.',
+                };
 
-                  ctrl.snapshots.import.loading = false;
-                  $uibModalInstance.close(result);
-                });
+                ctrl.snapshots.import.loading = false;
+                $uibModalInstance.close(result);
+              });
           }
 
         } else if (ctrl.options.which === 'csv') {
-            ctrl.checkCSVHeaders();
-            ctrl.checkCSVBody();
+          ctrl.checkCSVHeaders();
+          ctrl.checkCSVBody();
         }
 
         // check if any alerts defined.
@@ -182,113 +182,113 @@ angular.module('QuepidApp')
           ctrl.loading = true;
           if (ctrl.options.which === 'information_needs') {
             importRatingsSvc.importInformationNeeds(
-                ctrl.theCase,
-                ctrl.information_needs.content,
-                ctrl.createQueries
+              ctrl.theCase,
+              ctrl.information_needs.content,
+              ctrl.createQueries
             ).then(function () {
-                    ctrl.loading = false;
-                    let modalResponse = {
-                      success: true,
-                      message: 'Successfully imported information needs from CSV.'
-                    };
-                    $uibModalInstance.close(modalResponse);
-                  },
-                function (response) {
-                  let errorMessage = 'Unable to import information needs from CSV. ';
-                  if (response.data && response.data.message) {
-                    errorMessage += response.data.message;
-                  } else {
-                    errorMessage += response.status;
-                    errorMessage += ' - ' + response.statusText;
-                  }
-
-                  let modalResponse = {
-                    error: true,
-                    message: errorMessage.toString()
-                  };
-
-                  ctrl.loading = false;
-                  $uibModalInstance.close(modalResponse);
-                });
-         } else if (ctrl.options.which === 'csv') {
-
-              importRatingsSvc.importCSVFormat(
-                  ctrl.theCase,
-                  ctrl.csv.result,
-                  ctrl.clearQueries
-              ).then(function () {
-                    ctrl.loading = false;
-                    let modalResponse = {
-                      success: true,
-                      message: 'Successfully imported ratings from CSV.'
-                    };
-                    $uibModalInstance.close(modalResponse);
-                  },
-                  function (response) {
-                    let errorMessage = 'Unable to import ratings from CSV: ';
-                    errorMessage += response.status;
-                    errorMessage += ' - ' + response.statusText;
-
-                    let modalResponse = {
-                      error: true,
-                      message: errorMessage.toString()
-                    };
-
-                    ctrl.loading = false;
-                    $uibModalInstance.close(modalResponse);
-                  });
-            } else if (ctrl.options.which === 'rre') {
-              importRatingsSvc.importRREFormat(
-                  ctrl.theCase,
-                  ctrl.rre.content,
-                  ctrl.clearQueries
-              ).then(function () {
-                    ctrl.loading = false;
-                    let modalResponse = {
-                      success: true,
-                      message: 'Successfully imported ratings from RRE.'
-                    };
-                    $uibModalInstance.close(modalResponse);
-                  },
-                  function (response) {
-                    let errorMessage = 'Unable to import ratings from RRE: ';
-                    errorMessage += response.status;
-                    errorMessage += ' - ' + response.statusText;
-
-                    let modalResponse = {
-                      error: true,
-                      message: errorMessage.toString()
-                    };
-
-                    ctrl.loading = false;
-                    $uibModalInstance.close(modalResponse);
-                  });
-            } else if (ctrl.options.which === 'ltr') {
-            importRatingsSvc.importLTRFormat(
-                ctrl.theCase,
-                ctrl.ltr.content,
-                ctrl.clearQueries
-            ).then(function () {
-                  ctrl.loading = false;
-                  let modalResponse = {
-                    success: true,
-                    message: 'Successfully imported ratings from LTR.'
-                  };
-                  $uibModalInstance.close(modalResponse);
-                },
-                function (response) {
-                  let errorMessage = 'Unable to import ratings from LTR: ';
+                ctrl.loading = false;
+                let modalResponse = {
+                  success: true,
+                  message: 'Successfully imported information needs from CSV.'
+                };
+                $uibModalInstance.close(modalResponse);
+              },
+              function (response) {
+                let errorMessage = 'Unable to import information needs from CSV. ';
+                if (response.data && response.data.message) {
+                  errorMessage += response.data.message;
+                } else {
                   errorMessage += response.status;
                   errorMessage += ' - ' + response.statusText;
+                }
 
-                  let modalResponse = {
-                    error: true,
-                    message: errorMessage.toString()
-                  };
+                let modalResponse = {
+                  error: true,
+                  message: errorMessage.toString()
+                };
 
-                  ctrl.loading = false;
-                  $uibModalInstance.close(modalResponse);
-                });
+                ctrl.loading = false;
+                $uibModalInstance.close(modalResponse);
+              });
+          } else if (ctrl.options.which === 'csv') {
+
+            importRatingsSvc.importCSVFormat(
+              ctrl.theCase,
+              ctrl.csv.result,
+              ctrl.clearQueries
+            ).then(function () {
+                ctrl.loading = false;
+                let modalResponse = {
+                  success: true,
+                  message: 'Successfully imported ratings from CSV.'
+                };
+                $uibModalInstance.close(modalResponse);
+              },
+              function (response) {
+                let errorMessage = 'Unable to import ratings from CSV: ';
+                errorMessage += response.status;
+                errorMessage += ' - ' + response.statusText;
+
+                let modalResponse = {
+                  error: true,
+                  message: errorMessage.toString()
+                };
+
+                ctrl.loading = false;
+                $uibModalInstance.close(modalResponse);
+              });
+          } else if (ctrl.options.which === 'rre') {
+            importRatingsSvc.importRREFormat(
+              ctrl.theCase,
+              ctrl.rre.content,
+              ctrl.clearQueries
+            ).then(function () {
+                ctrl.loading = false;
+                let modalResponse = {
+                  success: true,
+                  message: 'Successfully imported ratings from RRE.'
+                };
+                $uibModalInstance.close(modalResponse);
+              },
+              function (response) {
+                let errorMessage = 'Unable to import ratings from RRE: ';
+                errorMessage += response.status;
+                errorMessage += ' - ' + response.statusText;
+
+                let modalResponse = {
+                  error: true,
+                  message: errorMessage.toString()
+                };
+
+                ctrl.loading = false;
+                $uibModalInstance.close(modalResponse);
+              });
+          } else if (ctrl.options.which === 'ltr') {
+            importRatingsSvc.importLTRFormat(
+              ctrl.theCase,
+              ctrl.ltr.content,
+              ctrl.clearQueries
+            ).then(function () {
+                ctrl.loading = false;
+                let modalResponse = {
+                  success: true,
+                  message: 'Successfully imported ratings from LTR.'
+                };
+                $uibModalInstance.close(modalResponse);
+              },
+              function (response) {
+                let errorMessage = 'Unable to import ratings from LTR: ';
+                errorMessage += response.status;
+                errorMessage += ' - ' + response.statusText;
+
+                let modalResponse = {
+                  error: true,
+                  message: errorMessage.toString()
+                };
+
+                ctrl.loading = false;
+                $uibModalInstance.close(modalResponse);
+              });
           }
         }
       };
@@ -297,9 +297,9 @@ angular.module('QuepidApp')
         $uibModalInstance.dismiss('cancel');
       };
 
-      ctrl.checkCSVHeaders = function() {
+      ctrl.checkCSVHeaders = function () {
         var headers = ctrl.csv.content.split('\n')[0];
-        headers     = headers.split(ctrl.csv.separator);
+        headers = headers.split(ctrl.csv.separator);
 
         var expectedHeaders = [
           'query', 'docid', 'rating'
@@ -314,20 +314,19 @@ angular.module('QuepidApp')
           ctrl.csv.import.alert = alert;
         }
       };
-      ctrl.checkCSVBody = function() {
+      ctrl.checkCSVBody = function () {
         var lines = ctrl.csv.content.split('\n');
         var i = 1;
         var alert;
         for (i = 1; i < lines.length; i++) {
           var line = lines[i];
-          if (line && line.split(ctrl.csv.separator).length > 3){
+          if (line && line.split(ctrl.csv.separator).length > 3) {
             var matches = line.match(/"/g);
-            if (matches !== undefined && matches !== null && matches.length === 2){
-             // two double quotes means we are okay
-            }
-            else {
+            if (matches !== undefined && matches !== null && matches.length === 2) {
+              // two double quotes means we are okay
+            } else {
               // check for wrapping in double quotes.
-              if (alert === undefined){
+              if (alert === undefined) {
                 alert = 'Must have three (or fewer) columns for every line in CSV file.  Make sure to wrap any queries with <code>,</code> in double quotes.';
                 alert += '<br /><strong>';
               }
@@ -337,15 +336,15 @@ angular.module('QuepidApp')
             }
           }
         }
-        if (alert !== undefined){
+        if (alert !== undefined) {
           alert += '</strong>';
           ctrl.csv.import.alert = alert;
         }
       };
 
-      ctrl.checkInformationNeedsHeaders = function() {
+      ctrl.checkInformationNeedsHeaders = function () {
         var headers = ctrl.information_needs.content.split('\n')[0];
-        headers     = headers.split(ctrl.information_needs.separator);
+        headers = headers.split(ctrl.information_needs.separator);
 
         var expectedHeaders = [
           'query', 'information_need'
@@ -361,19 +360,18 @@ angular.module('QuepidApp')
         }
       };
 
-      ctrl.checkInformationNeedsBody = function() {
+      ctrl.checkInformationNeedsBody = function () {
         var lines = ctrl.information_needs.content.split('\n');
         var i = 1;
         var alert;
         for (i = 1; i < lines.length; i++) {
           var line = lines[i];
-          if (line && line.split(ctrl.information_needs.separator).length > 2){
+          if (line && line.split(ctrl.information_needs.separator).length > 2) {
             var matches = line.match(/"/g);
-            if (matches !== undefined && matches !== null && matches.length >= 2){
+            if (matches !== undefined && matches !== null && matches.length >= 2) {
               // two double quotes (or more) means we are okay, it's not a perfect check
-            }
-            else {
-              if (alert === undefined){
+            } else {
+              if (alert === undefined) {
                 alert = 'Must have two (or fewer) columns for every line in CSV file.  Make sure to wrap any query and information_need with <code>,</code> in double quotes.';
                 alert += '<br /><strong>';
               }
@@ -383,11 +381,10 @@ angular.module('QuepidApp')
             }
           }
         }
-        if (alert !== undefined){
+        if (alert !== undefined) {
           alert += '</strong>';
           ctrl.information_needs.import.alert = alert;
         }
       };
-
     }
   ]);
