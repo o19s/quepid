4cb6b4e0e3547fae4c9453d9e67ff3fa4f94d614
Christopher Morley <depahelix@gmail.com>
2023-05-16 14:07:02 -0400

    clean up querySnapshotSvc.js debugging, and revert jupyter notebooks

--- app/assets/javascripts/services/querySnapshotSvc.js
+++ app/assets/javascripts/services/querySnapshotSvc.js
@@ -151,27 +151,23 @@ angular.module('QuepidApp')
       };
 
       function importSnapshotsToSpecificCase(docs, targetCaseNo) {
-        let docs2 = docs;
-        console.log('js 923847598w74987');
-        angular.forEach(docs2, function(doc) {
+        let docsWithCaseOverridden = docs;
+        angular.forEach(docsWithCaseOverridden, function(doc) {
           doc['Case ID'] = targetCaseNo;
         });
-        console.log('js 3049578398475');
-        return importSnapshots(docs2);
+        return importSnapshots(docsWithCaseOverridden);
       }
 
       function importSnapshots (docs) {
-        console.log('js 439879837459');
         var cases = {};
-        console.log('js 2309487698958');
+
         angular.forEach(docs, function(doc) {
-          console.log('js 3548793847598');
           if( !angular.isDefined(cases[doc['Case ID']]) ) {
             cases[doc['Case ID']] = { 'snapshots': {} };
           }
-          console.log('js 35487938475980398745698374');
+
           var aCase = cases[doc['Case ID']];
-          console.log('js 30539475987');
+
           if( !angular.isDefined(aCase.snapshots[doc['Snapshot Name']]) ) {
             var time = doc['Snapshot Time'];
             aCase.snapshots[doc['Snapshot Name']] = {
@@ -182,48 +178,42 @@ angular.module('QuepidApp')
           }
 
           var snapshot = aCase.snapshots[doc['Snapshot Name']];
-          console.log('js 4857349578');
+
           if( !angular.isDefined(snapshot.queries[doc['Query Text']]) ) {
             snapshot.queries[doc['Query Text']] = { 'docs': [] };
           }
-          console.log('js 3045973409857');
+
           var query = snapshot.queries[doc['Query Text']];
-          console.log('js 30497853498567');
+
           query.docs.push( { 'id': doc['Doc ID'], 'position': doc['Doc Position'] } );
-          console.log('js 98637954876552');
         });
-        console.log('js 30495679863759');
+
         function callApi (caseId, snapshotData) {
           var url = 'api/cases/' + caseId + '/snapshots/imports';
-          console.log('js 30495679863759');
           return $http.post(url, { snapshots: [snapshotData] })
             .then(function(response) {
-              console.log('js o3874567987495');
               return addSnapshotResp(response.data.snapshots);
             });
         }
 
         var deferred  = $q.defer();
         var promises  = [];
-        console.log('js 03976598475698');
+
         angular.forEach(cases, function(caseData, caseId) {
           angular.forEach(caseData.snapshots, function(snapshot) {
             promises.push(callApi(caseId, snapshot));
           });
         });
-        console.log('js 034985769485769');
+
         $q.all(promises)
           .then(function() {
-            console.log('js 49857694857698');
             deferred.resolve('Snapshots imported.');
           }, function(message) {
-            console.log('js 405396798457698');
             deferred.reject(message);
           });
-        console.log('js 398679485769485679');
+
         return deferred.promise;
       }
-
 
       function get(snapshotId) {
         var url     = 'api/cases/' + caseNo + '/snapshots/' + snapshotId+ '?shallow=true';
