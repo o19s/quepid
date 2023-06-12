abcfc492a7e27a0710a431a99168baa06a5493e5
Eric Pugh <epugh@opensourceconnections.com>
2023-06-12 01:45:28 -0400

    more work arounds for our legacy way to play nice

--- app/controllers/concerns/api_key_authenticatable.rb
+++ app/controllers/concerns/api_key_authenticatable.rb
@@ -9,17 +9,22 @@ module ApiKeyAuthenticatable
   attr_reader :current_api_key
   attr_reader :current_user
 
+  # both of the authentication_with_api_key methods need to work with our Devise based
+  # approach of using a Session.  So only kick this is when we don't have a current user (i.e no session)
+  # and we have an Authorization header being passed in.
+
   # Use this to raise an error and automatically respond with a 401 HTTP status
   # code when API key authentication fails
   def authenticate_with_api_key!
-    if @current_user.nil? # we may already have a current_user from cookies.
+
+    if @current_user.nil? && !request.headers['Authorization'].nil?
       @current_user = authenticate_or_request_with_http_token(&method(:authenticator))
     end
   end
 
   # Use this for optional API key authentication
   def authenticate_with_api_key
-    if @current_user.nil? # we may already have a current_user from cookies.
+    if @current_user.nil? && request.headers['Authorization'].nil?
       @current_bearer = authenticate_with_http_token(&method(:authenticator))
     end
   end
