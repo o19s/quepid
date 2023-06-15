50458a52407f192c8b0613d1cb43942713e28878
Eric Pugh <epugh@opensourceconnections.com>
2023-06-15 14:49:06 -0400

    oops

--- db/migrate/20230615131930_simplify_our_api_keys.rb
+++ db/migrate/20230615131930_simplify_our_api_keys.rb
@@ -1,14 +1,12 @@
 class SimplifyOurApiKeys < ActiveRecord::Migration[7.0]
   def change
-    drop_table :api_keys
-
     create_table :api_keys do |t|
       t.integer :user_id
-      t.string :token_digest, null: false 
+      t.string :token_digest
       t.timestamps
     end
 
 
-    add_index :api_keys, :token_digest #, unique: true    
+    add_index :api_keys, :token_digest    
   end
 end
