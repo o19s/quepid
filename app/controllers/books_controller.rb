50c229d7f7891be6e4143de43f182d9318b2a9fd
Eric Pugh <epugh@opensourceconnections.com>
2023-06-26 17:14:36 -0400

    checkpoint that we have a working UI

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -45,6 +45,15 @@ class BooksController < ApplicationController
     respond_with(@book)
   end
 
+  def combine
+    book_ids = params[:book_ids].select { |_key, value| '1' == value }.keys.map(&:to_i)
+    puts "I got params: #{book_ids}"
+    book_ids.each do |book_id|
+      book_to_merge = current_user.books_involved_with.where(id: book_id).first
+      puts "#{book_to_merge.name}: #{book_to_merge.query_doc_pairs.count} qdps, #{book_to_merge.judgements.count} j"
+    end
+  end
+
   private
 
   def find_book
