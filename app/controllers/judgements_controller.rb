f40973d7ee9999f11308a46efcf952fe831e4141
Eric Pugh <epugh@opensourceconnections.com>
2023-07-04 09:18:04 -0400

    Its a rating party! (#778)
    * its a party
    
    * leaderboard

--- app/controllers/judgements_controller.rb
+++ app/controllers/judgements_controller.rb
@@ -18,19 +18,48 @@ class JudgementsController < ApplicationController
   end
 
   # rubocop:disable Layout/LineLength
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/PerceivedComplexity
   def new
+    track_judging = session[:track_judging].symbolize_keys
+
+    track_judging = { book_id: @book.id, counter: 0 } if track_judging.nil? || (track_judging[:book_id] != @book.id)
+
+    track_judging[:counter] = track_judging[:counter] + 1
+    session[:track_judging] = track_judging
+
     @query_doc_pair = SelectionStrategy.random_query_doc_based_on_strategy(@book, current_user)
     redirect_to book_path(@book) if @query_doc_pair.nil? # no more query doc pairs to be judged!
     if @query_doc_pair
-      # @query = @current_user.queries.has_information_need.where(query_text: @query_doc_pair.query_text).first
       @query = Query.joins(:case).where(case: { book_id: @query_doc_pair.book.id }).has_information_need.where(query_text: @query_doc_pair.query_text).first
     end
     @judgement = Judgement.new(query_doc_pair: @query_doc_pair, user: @current_user, updated_at: Time.zone.now)
+    @previous_judgement = @judgement.previous_judgement_made unless @judgement.new_record?
+
+    if (track_judging[:counter] % 20).zero? # It's party time!
+      @party_time = true
+      @judged_by_user = @book.judgements.where(user: @current_user).count.to_f
+      @total_pool_of_judgements = @book.query_doc_pairs.count.to_f
+
+      @leaderboard_data = []
+      unique_judges = @book.judgements.rateable.preload(:user).collect(&:user).uniq
+      unique_judges.each do |judge|
+        @leaderboard_data << { judge: judge.nil? ? 'anonymous' : judge.name, judgements: @book.judgements.where(user: judge).count }
+      end
+
+    end
   end
   # rubocop:enable Layout/LineLength
+  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/PerceivedComplexity
 
   def edit
     @query_doc_pair = @judgement.query_doc_pair
+    @previous_judgement = @judgement.previous_judgement_made
   end
 
   def create
