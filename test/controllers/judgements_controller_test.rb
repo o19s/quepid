0a17761a5454021be6f8344d5dc755aeec35c6c5
Eric Pugh <epugh@opensourceconnections.com>
2023-08-31 19:55:52 -0400

    lint

--- test/controllers/judgements_controller_test.rb
+++ test/controllers/judgements_controller_test.rb
@@ -3,10 +3,9 @@
 require 'test_helper'
 
 class JudgementsControllerTest < ActionDispatch::IntegrationTest
-  
   let(:user) { users(:random) }
   let(:book) { books(:book_of_comedy_films) }
-  
+
   setup do
     @judgement = judgements(:one)
     get '/books'
@@ -21,47 +20,46 @@ class JudgementsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-#   test 'should get new' do
-#     get new_judgement_url
-#     assert_response :success
-#   end
+  #   test 'should get new' do
+  #     get new_judgement_url
+  #     assert_response :success
+  #   end
 
-#   test 'should create judgement' do
-#     assert_difference('Judgement.count') do
-#       post judgements_url,
-#            params: { judgement: { query_doc_pair_id: @judgement.query_doc_pair_id, rating: @judgement.rating,
-# user_id: @judgement.user_id } }
-#     end
+  #   test 'should create judgement' do
+  #     assert_difference('Judgement.count') do
+  #       post judgements_url,
+  #            params: { judgement: { query_doc_pair_id: @judgement.query_doc_pair_id, rating: @judgement.rating,
+  # user_id: @judgement.user_id } }
+  #     end
 
-#     assert_redirected_to judgement_url(Judgement.last)
-#   end
+  #     assert_redirected_to judgement_url(Judgement.last)
+  #   end
 
-#   test 'should show judgement' do
-#     get judgement_url(@judgement)
-#     assert_response :success
-#   end
+  #   test 'should show judgement' do
+  #     get judgement_url(@judgement)
+  #     assert_response :success
+  #   end
 
-#   test 'should get edit' do
-#     get edit_judgement_url(@judgement)
-#     assert_response :success
-#   end
+  #   test 'should get edit' do
+  #     get edit_judgement_url(@judgement)
+  #     assert_response :success
+  #   end
 
-#   test 'should update judgement' do
-#     patch judgement_url(@judgement),
-#           params: { judgement: { query_doc_pair_id: @judgement.query_doc_pair_id, rating: @judgement.rating,
-# user_id: @judgement.user_id } }
-#     assert_redirected_to judgement_url(@judgement)
-#   end
+  #   test 'should update judgement' do
+  #     patch judgement_url(@judgement),
+  #           params: { judgement: { query_doc_pair_id: @judgement.query_doc_pair_id, rating: @judgement.rating,
+  # user_id: @judgement.user_id } }
+  #     assert_redirected_to judgement_url(@judgement)
+  #   end
 
-#   test 'should destroy judgement' do
-#     assert_difference('Judgement.count', -1) do
-#       delete judgement_url(@judgement)
-#     end
+  #   test 'should destroy judgement' do
+  #     assert_difference('Judgement.count', -1) do
+  #       delete judgement_url(@judgement)
+  #     end
 
-#     assert_redirected_to judgements_url
-#   end
-  
-  
+  #     assert_redirected_to judgements_url
+  #   end
+
   def login_user
     # We don't actually want to load up scores...
     Bullet.enable = false
