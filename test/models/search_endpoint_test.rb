d5e37855f1f4b2101b1fdd86ac1155acb6f539cc
Eric Pugh <epugh@opensourceconnections.com>
2023-09-07 12:57:57 -0400

    docs...

--- test/models/search_endpoint_test.rb
+++ test/models/search_endpoint_test.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: search_endpoints
+#
+#  id             :bigint           not null, primary key
+#  api_method     :string(255)
+#  custom_headers :string(1000)
+#  endpoint_url   :string(500)
+#  name           :string(255)
+#  search_engine  :string(50)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  owner_id       :integer
+#
 require 'test_helper'
 
 class SearchEndpointTest < ActiveSupport::TestCase
