d0082dea22f4cdb1fc8ef5a07b3d3444b6da64a5
Eric Pugh <epugh@opensourceconnections.com>
2023-09-13 10:25:32 -0400

    be smarter about nils

--- db/migrate/20230816183004_create_search_endpoints_from_tries.rb
+++ db/migrate/20230816183004_create_search_endpoints_from_tries.rb
@@ -5,11 +5,12 @@ class CreateSearchEndpointsFromTries < ActiveRecord::Migration[7.0]
       # Go through and find each unique set of values from all the tries,
       # and create a single end point that is used by multiple tries where it
       # doesn't change.
+      
       search_endpoint = SearchEndpoint.find_or_create_by search_engine: try.search_engine,
         endpoint_url: try.search_url,
         api_method: try.api_method,
         custom_headers: try.custom_headers,
-        owner_id: try.case.owner_id,
+        owner: try.case.owner,
         name: "#{try.search_engine} #{try.search_url}"
       
       try.search_endpoint = search_endpoint
