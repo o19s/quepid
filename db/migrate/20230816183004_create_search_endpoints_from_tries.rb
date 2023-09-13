949f6ee1b7c2b0f88b6ad0c66a6511c37300f9cc
Eric Pugh <epugh@opensourceconnections.com>
2023-09-13 11:58:18 -0400

    skip the name

--- db/migrate/20230816183004_create_search_endpoints_from_tries.rb
+++ db/migrate/20230816183004_create_search_endpoints_from_tries.rb
@@ -8,16 +8,16 @@ class CreateSearchEndpointsFromTries < ActiveRecord::Migration[7.0]
       # and create a single end point that is used by multiple tries where it
       # doesn't change.
       
-      name = "#{try.search_engine} #{try.search_url}"
+      #name = "#{try.search_engine} #{try.search_url}"
       
-      name = name.truncate(30, omission: "#{name.first(15)}...#{name.last(15)}")
+      #name = name.truncate(30, omission: "#{name.first(15)}...#{name.last(15)}")
       
       search_endpoint = SearchEndpoint.find_or_create_by search_engine: try.search_engine,
         endpoint_url: try.search_url,
         api_method: try.api_method,
         custom_headers: try.custom_headers,
-        owner: try.case.owner,
-        name: name
+        owner: try.case.owner
+       # name: name
       
       try.search_endpoint = search_endpoint
       
