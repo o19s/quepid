fe55582879bbcddcbe71e1be200d5eed677874b3
Eric Pugh <epugh@opensourceconnections.com>
2023-09-13 10:47:57 -0400

    once more

--- db/migrate/20230816183004_create_search_endpoints_from_tries.rb
+++ db/migrate/20230816183004_create_search_endpoints_from_tries.rb
@@ -3,9 +3,13 @@ class CreateSearchEndpointsFromTries < ActiveRecord::Migration[7.0]
     
     # Discovered that there are tries that have null case's from 2016.  We need to clean this up.
     
+    puts "Found #{Try.all.where(case_id: nil).count} bogus tries to destroy."
+    
     Try.all.where(case_id: nil) do |try|
       t.destroy!
     end
+    
+    puts "Now, found #{Try.all.where(case_id: nil).count} bogus tries to destroy."
     
     
     Try.all.each do |try|
