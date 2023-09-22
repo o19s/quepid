0a8433ca97e444d91a98262eb497b4f3e5e1b654
Eric Pugh <epugh@opensourceconnections.com>
2023-09-22 09:29:19 -0400

    fixes

--- app/assets/javascripts/services/settingsSvc.js
+++ app/assets/javascripts/services/settingsSvc.js
@@ -304,7 +304,7 @@ angular.module('QuepidApp')
       
       this.getDemoSettings = function(searchEngine){
         return angular.copy(this.tmdbSettings[searchEngine]);
-      };
+    };
 
       this.setCaseTries = function(tries) {
         currSettings = new Settings(tries);
