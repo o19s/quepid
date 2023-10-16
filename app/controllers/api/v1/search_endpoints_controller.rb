443e5338fd4ce5132bf70b5dac7a12b7343a418d
Eric Pugh <epugh@opensourceconnections.com>
2023-10-16 16:38:35 -0400

    stuff

--- app/controllers/api/v1/search_endpoints_controller.rb
+++ app/controllers/api/v1/search_endpoints_controller.rb
@@ -6,7 +6,7 @@ module Api
       before_action :set_search_endpoint, only: [ :show, :update ]
       def index
         bool = ActiveRecord::Type::Boolean.new
-        archived = bool.deserialize params[:archived]
+        archived = bool.deserialize params[:archived] || false
 
         current_user.search_endpoints_involved_with
 
