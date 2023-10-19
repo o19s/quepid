7249b8805d52dc43bc757b2df3c96f597fb7ba29
Eric Pugh <epugh@opensourceconnections.com>
2023-10-19 08:17:59 -0400

    lint

--- app/controllers/analytics/cases/duplicate_scores_controller.rb
+++ app/controllers/analytics/cases/duplicate_scores_controller.rb
@@ -9,7 +9,7 @@ module Analytics
         @duplicate_score_patterns = Score.select([ 'count(*) as count', :try_id, :score, 'DATE(updated_at) AS day' ])
           .where(case_id: @case.id)
           .scored.group(
-            :try_id, :score,'day'
+            :try_id, :score, 'day'
           ).having('COUNT(*) > 0')
       end
     end
@@ -17,7 +17,7 @@ module Analytics
 end
 
 # script for finding and clearing out all the duplicate scores.
-# 
+#
 
 # Case.all.find_each do |kase|
 #   puts "Here we go for Case #{kase.id}, #{kase.case_name}"
@@ -26,11 +26,11 @@ end
 #     .scored.group(
 #       :try_id, 'day'
 #     ).having('COUNT(*) > 1')
-  
+
 #   @duplicate_score_patterns.each do |dsp|
-#     puts "try #{dsp.try_id} on #{dsp.day}: #{dsp.count}"  
+#     puts "try #{dsp.try_id} on #{dsp.day}: #{dsp.count}"
 #   end
-  
+
 #   @duplicate_score_patterns.each do |dsp|
 #     puts "#{dsp.day}: #{dsp.count}, #{dsp.try_id}"
 #     scores = Score.where(case_id: kase.id).where(try_id: dsp.try_id).where('DATE(updated_at) = :day', day: dsp.day )
