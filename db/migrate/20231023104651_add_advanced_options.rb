92fd430d5eab5998dc9c9c7f4b4b953f8a0a4353
Eric Pugh <epugh@opensourceconnections.com>
2023-10-25 10:45:39 -0400

    Gah!  chatgpt gave me bad advice, and every basic auth had the value "0"

--- db/migrate/20231023104651_add_advanced_options.rb
+++ db/migrate/20231023104651_add_advanced_options.rb
@@ -1,6 +1,5 @@
 class AddAdvancedOptions < ActiveRecord::Migration[7.0]
   def change
-    #add_column :search_endpoints, :proxy_requests, :boolean, default: false
-    add_column :search_endpoints, :basic_auth_credential, :string, default: false
+    add_column :search_endpoints, :basic_auth_credential, :string
   end
 end
