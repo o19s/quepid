d9f336a8d91b29ec155f75bdd85597302f9969fa
Eric Pugh <epugh@opensourceconnections.com>
2023-11-20 09:22:50 -0500

    new version of rails!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_11_181543) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_11_181543) do
   create_table "annotations", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.text "message"
     t.string "source"
