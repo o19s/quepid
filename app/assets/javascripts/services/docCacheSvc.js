032b17fc9f9c7a948a9ae04cd5289b7bc48c5456
Eric Pugh <epugh@opensourceconnections.com>
2023-11-30 11:49:47 -0500

    Deal with some search endpoints do not support lookup By Id

--- app/assets/javascripts/services/docCacheSvc.js
+++ app/assets/javascripts/services/docCacheSvc.js
@@ -60,8 +60,7 @@ angular.module('QuepidApp')
         }
         
         var docIds    = Object.keys(docsToFetch);
-        var resolver  = docResolverSvc.createResolver(docIds, settings, 15);
-
+        
         // 'vectara' does not support doc lookup by ID.
         let supportLookupById = true;
         if (settings && settings.searchEngine === 'vectara'){
@@ -73,6 +72,8 @@ angular.module('QuepidApp')
       
         
         if ( supportLookupById && docIds.length > 0 ) { 
+          var resolver  = docResolverSvc.createResolver(docIds, settings, 15);
+
           return resolver.fetchDocs()
             .then(function () {
               angular.forEach(resolver.docs, function (doc) {
