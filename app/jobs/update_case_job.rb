350b37f200840a9b2e3e9ce1a3b6f225952ed9a4
Eric Pugh <epugh@opensourceconnections.com>
2023-12-08 10:56:58 -0500

    Ooops!  lots of extra queries!

--- app/jobs/update_case_job.rb
+++ app/jobs/update_case_job.rb
@@ -6,10 +6,8 @@ class UpdateCaseJob < ApplicationJob
   def perform book
     service = RatingsManager.new(book)
 
-    book.query_doc_pairs.each do |_query_doc_pair|
-      book.cases.each do |kase|
-        service.sync_ratings_for_case(kase)
-      end
+    book.cases.each do |kase|
+      service.sync_ratings_for_case(kase)
     end
   end
 end
