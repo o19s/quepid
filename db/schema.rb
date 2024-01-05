c86dcd0cfe723d939097dff0acc04f5ce3d62c48
Eric Pugh <epugh@opensourceconnections.com>
2024-01-05 14:38:44 -0500

    clean up missing owners!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_03_175043) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_05_193323) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
