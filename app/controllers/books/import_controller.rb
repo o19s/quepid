bded237b6f08ba243f05897f946e80d37a09dad4
Eric Pugh <epugh@opensourceconnections.com>
2024-01-08 10:32:59 -0500

    unused method

--- app/controllers/books/import_controller.rb
+++ app/controllers/books/import_controller.rb
@@ -41,48 +41,5 @@ module Books
     end
     # rubocop:enable Metrics/MethodLength
     # rubocop:enable Security/Open
-
-    def create2
-      @book = Book.new
-      @book.owner = current_user
-
-      uploaded_file = params[:book][:json_upload]
-      json_file = uploaded_file.tempfile
-      json_data = URI.open(json_file) do |file|
-        JSON.parse(file.read)
-      end
-
-      begin
-        params_to_use = JSON.parse(json_data.read).deep_symbolize_keys
-
-        @book.name = params_to_use[:name]
-        scorer_name = params_to_use[:scorer][:name]
-        scorer = Scorer.find_by(name: scorer_name)
-        if scorer.nil?
-          @book.errors.add(:scorer, "Scorer with name '#{scorer_name}' needs to be migrated over first.")
-        else
-          @book.scorer = scorer
-        end
-
-        selection_strategy_name = params_to_use[:selection_strategy][:name]
-        selection_strategy = SelectionStrategy.find_by(name: selection_strategy_name)
-        if selection_strategy.nil?
-          @book.errors.add(:selection_strategy,
-                           "Selection strategy with name '#{selection_strategy_name}' needs to be migrated over first.")
-        else
-          @book.selection_strategy = selection_strategy
-        end
-
-        @book.json_upload.attach(uploaded_file)
-      rescue JSON::ParserError => e
-        @book.errors.add(:base, "Invalid JSON file format: #{e.message}")
-      end
-
-      if @book.errors.empty? && @book.save
-        redirect_to @book, notice: 'Book was successfully created.'
-      else
-        render :new
-      end
-    end
   end
 end
