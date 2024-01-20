ee27bc51950e675d76c21812f474bffab4ef6960
Eric Pugh <epugh@opensourceconnections.com>
2024-01-20 10:44:19 -0500

    found in prod an example of a se with no tries

--- db/migrate/20240105193323_ensure_owner_id_for_search_endpoints.rb
+++ db/migrate/20240105193323_ensure_owner_id_for_search_endpoints.rb
@@ -1,12 +1,14 @@
 class EnsureOwnerIdForSearchEndpoints < ActiveRecord::Migration[7.1]
-  def change
-    
+  def change    
     # Discoverd that there are search endpoints being created without an owner!
     
     puts "Found #{SearchEndpoint.all.where(owner_id: nil).count} search endpoints missing owner"
     
     SearchEndpoint.all.where(owner_id: nil).each do |se|
-      se.owner = se.tries.last.case.owner
+      # only do this where we have a try related...
+      if se.tries.last
+        se.owner = se.tries.last.case.owner      
+      end
       se.save!
     end
   end
