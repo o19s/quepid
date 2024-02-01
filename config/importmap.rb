aa7b41da4082abbd6ecf9f89172b1215a5131e4c
Eric Pugh <epugh@opensourceconnections.com>
2024-02-01 15:55:46 -0500

    weird thing about naming but now it works...

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 # Pin npm packages by running ./bin/importmap
-pin 'local-time', preload: true # @3.0.2
 
 pin 'application2', preload: true
+pin "local-time", to: "vendored-local-time.js"# @3.0.2
