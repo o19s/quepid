4816ea3a93a2e2beb2816162f0af1a7536a07b7f
Eric Pugh <epugh@opensourceconnections.com>
2024-02-01 15:28:06 -0500

    not quite there yet!

--- app/javascript/application2.js
+++ app/javascript/application2.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// This is a importmap version of the classic application.js so that we can have
+// both sprockets and importmaps at the same time.
 import LocalTime from "local-time"
 LocalTime.start()
