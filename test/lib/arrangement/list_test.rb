85216b2b4d8b7ba7450d585b7b71f619c8fac668
Eric Pugh <epugh@opensourceconnections.com>
2024-03-27 22:05:09 -0400

    fix the path

--- test/lib/arrangement/list_test.rb
+++ test/lib/arrangement/list_test.rb
@@ -2,7 +2,7 @@
 
 require 'test_helper'
 require 'ostruct'
-require 'arrangement/list'
+#require_relative 'arrangement/list'
 
 # rubocop:disable Style/StructInheritance
 class Node < Struct.new(:id, :arranged_at, :arranged_next)
