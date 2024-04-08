17802eaf80f2a31f67e6b206b8eb13c3ec3d49c8
Eric Pugh <epugh@opensourceconnections.com>
2024-04-08 18:09:21 -0400

    small bug fix for when you have not yet picked a Case!

--- app/assets/javascripts/controllers/headerCtrl.js
+++ app/assets/javascripts/controllers/headerCtrl.js
@@ -70,12 +70,14 @@ angular.module('QuepidApp')
       });
       
       function createBookLink() {
+        let bookLink = 'books/new';
         if ($scope.theCase){
           const teamIds = $scope.theCase.teams.map(function(team) {
             return `&team_ids[]=${team.id}`;
           });
-          return `books/new?book[scorer_id]=${$scope.theCase.scorerId}${teamIds}&origin_case_id=${$scope.theCase.caseNo}`;
-        }      
+          bookLink = `${bookLink}?book[scorer_id]=${$scope.theCase.scorerId}${teamIds}&origin_case_id=${$scope.theCase.caseNo}`;
+        } 
+        return bookLink;
       }
     }
   ]);
