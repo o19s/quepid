53a75ccc6302fec9bd5481af729fa0e03d1be6a6
Eric Pugh <epugh@opensourceconnections.com>
2024-05-31 14:57:41 -0400

    fix test

--- app/controllers/admin/websocket_tester_controller.rb
+++ app/controllers/admin/websocket_tester_controller.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 
-class WebsocketTesterController < ApplicationController
-  def index
-    @action_cable_settings = {
-      'config/cable.yml'          => Rails.configuration.action_cable.to_hash,
-      'config/environments/*.rb'  => {
-        development: Rails.configuration.action_cable.to_hash,
-        test:        Rails.configuration.action_cable.to_hash,
-        production:  Rails.configuration.action_cable.to_hash,
-      },
-      'app/channels/*_channel.rb' => {
-        # You can introspect your channels here
-      },
-    }
-  end
-
-  def test_background_job
-    SimulateBackgroundJob.perform_later
-    redirect_to websocket_tester_index_path, notice: 'Test Background Job was queued up.'
+module Admin
+  class WebsocketTesterController < ApplicationController
+    def index
+      @action_cable_settings = {
+        'config/cable.yml'          => Rails.configuration.action_cable.to_hash,
+        'config/environments/*.rb'  => {
+          development: Rails.configuration.action_cable.to_hash,
+          test:        Rails.configuration.action_cable.to_hash,
+          production:  Rails.configuration.action_cable.to_hash,
+        },
+        'app/channels/*_channel.rb' => {
+          # You can introspect your channels here
+        },
+      }
+    end
+  
+    def test_background_job
+      SimulateBackgroundJob.perform_later
+      redirect_to admin_websocket_tester_index_path, notice: 'Test Background Job was queued up.'
+    end
   end
 end
