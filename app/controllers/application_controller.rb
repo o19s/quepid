a7fd45916909f6a7b8b746095a138bdf8c8006b1
Eric Pugh <epugh@opensourceconnections.com>
2024-05-31 15:43:34 -0400

    back out some changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,12 +12,11 @@ class ApplicationController < ActionController::Base
 
   respond_to :html, :js
 
-  # trying to work around actioncable
   before_action :set_current_user
   before_action :require_login
   before_action :check_current_user_locked!
   before_action :check_for_announcement
-  skip_before_action :require_login, if: -> { 'cable' == action_name }
+  #skip_before_action :require_login, if: -> { 'cable' == action_name }
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
