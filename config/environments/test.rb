034931d2d283ad979c385a3e852afb270a7df068
Eric Pugh <epugh@opensourceconnections.com>
2024-11-05 12:02:59 -0500

    roll

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -56,9 +56,9 @@ Rails.application.configure do
   config.log_level = :debug
 
   config.after_initialize do
-    Bullet.enable = true
-    Bullet.bullet_logger = true
-    Bullet.raise = true # raise an error if n+1 query occurs
+    # Bullet.enable = true
+    # Bullet.bullet_logger = true
+    # Bullet.raise = true # raise an error if n+1 query occurs
   end
 
   # Raises error for missing translations
