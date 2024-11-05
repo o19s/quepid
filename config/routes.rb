ef9641acaf33280a643ed3a98d31096cafc0d9bc
Eric Pugh <epugh@opensourceconnections.com>
2024-11-05 15:16:16 -0500

    Updates from RailsDiff for 8.0.0.rc2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@
 
 # rubocop:disable Metrics/BlockLength
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
   apipie
   mount ActiveStorageDB::Engine => '/active_storage_db'
   Healthcheck.routes(self)
