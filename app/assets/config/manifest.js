80f3e62701b8cf9eb176890fe9e7c7781fff5807
Eric Pugh <epugh@opensourceconnections.com>
2024-11-07 08:52:20 -0500

    once more...

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,3 +3,4 @@
 //= link 'application.css'
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link @rails/actioncable/app/assets/javascripts/actioncable.js
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  mount ActionCable.server => '/cable'
   apipie
   mount ActiveStorageDB::Engine => '/active_storage_db'
   get 'healthcheck' => 'rails/health#show', as: :rails_health_check
