bc7ee0f942acc66d96e9893e7315fd570caf6a01
Eric Pugh <epugh@opensourceconnections.com>
2024-12-09 17:59:17 -0500

    maybe?

--- app/jobs/enqueue_nightly_query_runner_job.rb
+++ app/jobs/enqueue_nightly_query_runner_job.rb
@@ -3,7 +3,7 @@
 class EnqueueNightlyQueryRunnerJob < ApplicationJob
   queue_as :default
 
-  def perform
+  def perform(*args)
     Case.all.nightly_run.each do |kase|
       try = kase.tries.last
       QueryRunnerJob.perform_later kase, try
