d02be9636fff563ff116f85f7bc8f26fdd42fe01
Eric Pugh <epugh@opensourceconnections.com>
2024-12-10 15:47:44 -0500

    forgot to remove this attempt

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,7 +3,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin 'application2', preload: true
-pin 'home'
 pin 'local-time', to: 'vendored-local-time.js' # @3.0.2
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 
