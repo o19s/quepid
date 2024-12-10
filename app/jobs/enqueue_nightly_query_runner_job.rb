0f68a025dd05c70f9b3fce1d514353e7b9ed0a99
Eric Pugh <epugh@opensourceconnections.com>
2024-12-10 12:38:30 -0500

    fix the sorting of the try

--- app/jobs/enqueue_nightly_query_runner_job.rb
+++ app/jobs/enqueue_nightly_query_runner_job.rb
@@ -5,7 +5,7 @@ class EnqueueNightlyQueryRunnerJob < ApplicationJob
 
   def perform(*args)
     Case.all.nightly_run.each do |kase|
-      try = kase.tries.last
+      try = kase.tries.first # new to old ;-)
       QueryRunnerJob.perform_later kase, try
     end
   end
