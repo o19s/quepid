627e5999e4dd780930f6b6dfb4c0543070828581
Eric Pugh <epugh@opensourceconnections.com>
2024-12-20 18:22:12 -0500

    whtiespace

--- db/migrate/20241218173819_reset_all_book_cache_counters.rb
+++ db/migrate/20241218173819_reset_all_book_cache_counters.rb
@@ -2,16 +2,11 @@ class ResetAllBookCacheCounters < ActiveRecord::Migration[8.0]
   def up
   
    Book.all.each do |book|
-  
-       Book.reset_counters(book.id, :query_doc_pairs)
-  
-       end
-  
+       Book.reset_counters(book.id, :query_doc_pairs)  
+       end  
     end
   
-    def down
-  
-       # no rollback needed
-  
+    def down  
+       # no rollback needed  
     end
 end
