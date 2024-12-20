a8172a9d2d77a1f4a5de6bd8c55e4063779b79cd
Eric Pugh <epugh@opensourceconnections.com>
2024-12-20 18:21:32 -0500

    Dead code

--- app/models/case.rb
+++ app/models/case.rb
@@ -185,13 +185,6 @@ class Case < ApplicationRecord
     Rails.application.message_verifier('magic').generate(id)
   end
 
-  def self.enqueue_nightly_query_runner_jobs
-    Case.all.nightly_run.each do |kase|
-      try = kase.tries.last
-      QueryRunnerJob.perform_later kase, try
-    end
-  end
-
   private
 
   def set_scorer
