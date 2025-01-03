2d8e54d4c0d114fd344e5a8f59297847b7662f3c
David Fisher <dfisher@opensourcesolutions.com>
2025-01-03 11:44:32 -0500

    Address javascript deepsource warnings.

--- db/scorers/err@10.js
+++ db/scorers/err@10.js
@@ -1,10 +1,10 @@
 let k = 10 // @Rank
 // if less than K results, need to reduce K now or final score is too low
 k = numReturned() < k ? numReturned() : k
-let scores = Array(k);
-let missing_rating = 0; // pessimistic assumption
-
-for (var i = 0; i < k; i++) {
+const scores = Array(k);
+const missing_rating = 0; // pessimistic assumption
+let i = 0
+for (i = 0; i < k; i++) {
   if (hasDocRating(i)) {
     scores[i] = (docRating(i));
   } else {
@@ -16,12 +16,12 @@ function gain(grade, maxGrade=3.0) {
     return (Math.pow(2,grade) - 1.0)/Math.pow(2,maxGrade)
 }
 function err(lst, maxG=3.0) {
-    var ERR = 0.0
-    var trust = 1.0
-    for (var i = 0; i < lst.length; i++) {
-        var rank = i + 1.0
-        var pUseful = gain(lst[i], maxG)
-        var disc = pUseful/rank
+    let ERR = 0.0
+    let trust = 1.0
+    for (i = 0; i < lst.length; i++) {
+        let rank = i + 1.0
+        let pUseful = gain(lst[i], maxG)
+        let disc = pUseful/rank
         ERR = ERR + trust * disc
         trust = trust * (1.0 - pUseful)
     }
