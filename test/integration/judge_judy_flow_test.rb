aee39865d1d733cec6e6f5bd75afd10b7baa57a9
Eric Pugh <epugh@opensourceconnections.com>
2025-01-07 23:17:47 -0500

    works like this?

--- test/integration/judge_judy_flow_test.rb
+++ test/integration/judge_judy_flow_test.rb
@@ -18,7 +18,7 @@ class JudgeJudyFlowTest < ActionDispatch::IntegrationTest
     assert_includes acase.teams, team
     assert_includes book.teams, team
 
-    assert_equal 8, book.query_doc_pairs.size
+    assert_equal 7, book.query_doc_pairs.size
 
     # Set up Judge Judy.  Give her a prompt (and an OPENAI KEY)
     judge_judy = User.new name: 'judge judy', email: 'judgejudy@quepid.com', password: 'password'
