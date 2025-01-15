6d98540d5a6e1ef40df0963bd6e127e0355ba9b6
Eric Pugh <epugh@opensourceconnections.com>
2025-01-15 09:26:17 -0500

    Unused variable

--- app/jobs/populate_book_job.rb
+++ app/jobs/populate_book_job.rb
@@ -6,10 +6,9 @@ class PopulateBookJob < ApplicationJob
   # rubocop:disable Security/MarshalLoad
   # rubocop:disable Metrics/MethodLength
   # rubocop:disable Metrics/AbcSize
-  def perform book, kase, _user
+  def perform book, kase
     # down the road we should be using ActiveRecord-import and first_or_initialize instead.
     # See how snapshots are managed.
-    book.query_doc_pairs.empty?
 
     book.update(populate_job: "populate started at #{Time.zone.now}")
     compressed_data = book.populate_file.download
@@ -52,9 +51,6 @@ class PopulateBookJob < ApplicationJob
     book.populate_file.purge
     book.populate_job = nil
     book.save
-
-    # throwing an exception in tests..
-    # Analytics::Tracker.track_query_doc_pairs_bulk_updated_event user, book, is_book_empty
   end
   # rubocop:enable Security/MarshalLoad
   # rubocop:enable Metrics/MethodLength
