0150b2f80b363d2d4da0e886277b2f7f60cb0db1
Eric Pugh <epugh@opensourceconnections.com>
2025-01-15 17:38:22 -0500

    Update heroku.rb

--- config/initializers/heroku.rb
+++ config/initializers/heroku.rb
@@ -2,13 +2,15 @@
 
 # Unpack Jupyterlite on Heroku as part of start up
 # See assets.rake for some related code.
+puts "about to test out heroku."
 if ENV['HEROKU_APP_NAME'].present?
   notebooks_gz = Rails.root.join('notebooks.gz')
   destination = Rails.public_path
   notebooks_dir = Rails.public_path.join('notebooks')
   unless File.exist?(notebooks_dir)
     puts "Unpacking Jupyterlite into #{destination}"
-    system "tar -xzf #{notebooks_gz} --directory #{destination}"
+    #system "tar -xzf #{notebooks_gz} --directory #{destination}"
+    system "tar -xzf #{notebooks_gz} --directory ./eric"
 
     File.delete(notebooks_gz)
   end
