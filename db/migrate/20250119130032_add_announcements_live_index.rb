ea10f070890623b6061b9388efa693ddfad5dd5c
Eric Pugh <epugh@opensourceconnections.com>
2025-02-02 09:42:39 -0500

    we can have lots of non unique not live announcments

--- db/migrate/20250119130032_add_announcements_live_index.rb
+++ db/migrate/20250119130032_add_announcements_live_index.rb
@@ -1,5 +1,5 @@
 class AddAnnouncementsLiveIndex < ActiveRecord::Migration[8.0]
   def change
-    add_index :announcements, :live, name: :index_announcements_live, unique: true
+    add_index :announcements, :live, name: :index_announcements_live
   end
 end
