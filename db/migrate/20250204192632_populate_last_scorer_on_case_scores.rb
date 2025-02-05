c318e9103331be1d0064a32480298faa3d0a8045
Eric Pugh <epugh@opensourceconnections.com>
2025-02-05 12:38:22 -0500

    okay, lets set all null to the last scorer

--- db/migrate/20250204192632_populate_last_scorer_on_case_scores.rb
+++ db/migrate/20250204192632_populate_last_scorer_on_case_scores.rb
@@ -2,9 +2,8 @@ class PopulateLastScorerOnCaseScores < ActiveRecord::Migration[8.0]
   # We can't assume that all the scores are from the same scorer, but we can assume the most recent used the current Case Scorer
   def up
     Case.find_each do |kase|      
-      last_score = kase.last_score
-      unless last_score.blank?   
-        last_score.update_column(:scorer_id, kase.scorer.id)   
+      if kase.scorer.present?
+        kase.scores.update_all(:scorer_id, kase.scorer.id)   
       end
     end
   end
