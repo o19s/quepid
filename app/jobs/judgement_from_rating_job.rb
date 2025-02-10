ed85c087b71a66872871d7735f1f9773776ea802
Eric Pugh <epugh@opensourceconnections.com>
2025-02-10 16:16:10 -0500

    add comments about why this is the way it is...

--- app/jobs/judgement_from_rating_job.rb
+++ app/jobs/judgement_from_rating_job.rb
@@ -8,9 +8,19 @@ class JudgementFromRatingJob < ApplicationJob
     book = query.case.book
     if book
       query_doc_pair = book.query_doc_pairs.find_or_create_by query_text: rating.query.query_text, doc_id: rating.doc_id
-      query.
 
-        judgement = query_doc_pair.judgements.find_or_initialize_by(user: user)
+      # We can't populate the query_doc_pair.document_fields since we don't have that data
+      # but there are other fields we CAN update.
+      # query_doc_pair.document_fields = pair[:document_fields].to_json
+      
+      # We need to think about the difference between a query / rating and a query_doc_pair.
+      # query_doc_pair.information_need = query.information_need
+      # query_doc_pair.notes = query.notes
+      # query_doc_pair.options = query.options
+      
+      query_doc_pair.save!
+      
+      judgement = query_doc_pair.judgements.find_or_initialize_by(user: user)
       judgement.rating = rating.rating
       judgement.save!
     end
