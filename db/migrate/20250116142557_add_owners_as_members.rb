2e365cc9986026600674905ef42be3460488234e
Eric Pugh <epugh@opensourceconnections.com>
2025-02-11 08:18:20 -0500

    Deal with owner relationship removed before this migration runs

--- db/migrate/20250116142557_add_owners_as_members.rb
+++ db/migrate/20250116142557_add_owners_as_members.rb
@@ -1,10 +1,16 @@
 class AddOwnersAsMembers < ActiveRecord::Migration[8.0]
+  # the awkward use of SQL is because when the migration runs, we've already
+  # dropped the "owner" relationship from the team
   def change
     teams = Team.all
     teams.each do |team|
-      owner = team.owner
+      results = ActiveRecord::Base.connection.execute(
+        "SELECT owner_id FROM teams WHERE id = #{team.id}"
+      )
+      owner_id = results.first.first
+      owner = User.find(owner_id)
       unless team.members.include?(owner)
-        puts "Updating team #{team.name} for new member"
+        puts "Adding previous owner #{owner.name} to team #{team.name} as new member"
         team.members << owner
         team.save!
       end
