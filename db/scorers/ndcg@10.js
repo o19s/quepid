bd963f93d75898c01b6adc406e42a31e8eb7cfe2
David Fisher <dfisher@opensourcesolutions.com>
2025-02-24 15:19:16 -0500

    Javascript deepsource fixes.

--- db/scorers/ndcg@10.js
+++ db/scorers/ndcg@10.js
@@ -1,14 +1,14 @@
 const topK = 10 // @Rank
 const missing_rating = 0; // pessimistic assumption
 const scores = Array(topK);
-const ideal = []
+const idealScores = []
 eachDocWithRating(function(doc) {
-    ideal.push(doc.rating)
+    idealScores.push(doc.rating)
 })
-ideal.sort(function(a,b) { return b - a; });
+idealScores.sort(function(a,b) { return b - a; });
 for (var i = 0; i < topK; i++) {
-  if (!ideal[i]) {
-    ideal[i] = missing_rating;
+  if (!idealScores[i]) {
+    idealScores[i] = missing_rating;
   }
   if (hasDocRating(i)) {
     scores[i] = (docRating(i));
@@ -19,8 +19,8 @@ for (var i = 0; i < topK; i++) {
 function DCG(vals, k) {
   let dcg = 0;
   for (var j = 0; j < k; j++) {
-    let den = Math.log2(j + 2);
-    let num = Math.pow(2, vals[j]) - 1;
+    const den = Math.log2(j + 2);
+    const num = Math.pow(2, vals[j]) - 1;
     dcg += den ? (num / den) : 0;
   }
   return dcg;
@@ -30,4 +30,4 @@ function nDCG(vals, ideal, k) {
   const den = DCG(ideal, ideal.length);
   return den ? (num / den) : 0;
 }
-setScore(nDCG(scores, ideal, topK));
+setScore(nDCG(scores, idealScores, topK));
