8f080310cb8118f5db69c70adbda43c0ca985175
David Fisher <dfisher@opensourcesolutions.com>
2025-02-24 15:14:28 -0500

    Javascript deepsource fixes.

--- db/scorers/ndcg@10.js
+++ db/scorers/ndcg@10.js
@@ -1,13 +1,12 @@
-const k = 10 // @Rank                                                             
-const missing_rating = 0; // pessimistic assumption                               
-let scores = Array(k);
-let ideal = []
+const topK = 10 // @Rank
+const missing_rating = 0; // pessimistic assumption
+const scores = Array(topK);
+const ideal = []
 eachDocWithRating(function(doc) {
     ideal.push(doc.rating)
 })
 ideal.sort(function(a,b) { return b - a; });
-//console.log(ideal)                                                            
-for (var i = 0; i < k; i++) {
+for (var i = 0; i < topK; i++) {
   if (!ideal[i]) {
     ideal[i] = missing_rating;
   }
@@ -17,21 +16,18 @@ for (var i = 0; i < k; i++) {
     scores[i] = missing_rating;
   }
 }
-
 function DCG(vals, k) {
   let dcg = 0;
   for (var j = 0; j < k; j++) {
-    const d = Math.log2(j + 2);
-    const n = Math.pow(2, vals[j]) - 1;
-    dcg += d ? (n / d) : 0;
+    let den = Math.log2(j + 2);
+    let num = Math.pow(2, vals[j]) - 1;
+    dcg += den ? (num / den) : 0;
   }
   return dcg;
 }
-
 function nDCG(vals, ideal, k) {
-  const n = DCG(vals, k);
-  const d = DCG(ideal, ideal.length);
-  return d ? (n / d) : 0;
+  const num = DCG(vals, k);
+  const den = DCG(ideal, ideal.length);
+  return den ? (num / den) : 0;
 }
-
-setScore(nDCG(scores, ideal, k));
+setScore(nDCG(scores, ideal, topK));
