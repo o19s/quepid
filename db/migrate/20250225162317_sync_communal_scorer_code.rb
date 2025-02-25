f1906234b0657976d14b4528aa2bcec8ea57287d
Eric Pugh <epugh@opensourceconnections.com>
2025-02-25 11:53:42 -0500

    fix for staging and prod

--- db/migrate/20250225162317_sync_communal_scorer_code.rb
+++ db/migrate/20250225162317_sync_communal_scorer_code.rb
@@ -1,7 +1,10 @@
 class SyncCommunalScorerCode < ActiveRecord::Migration[8.0]
   def change    
     Scorer.communal.each do |scorer|
-      file_name = scorer.name.downcase + '.js'    
+      file_name = scorer.name.downcase + '.js'  
+      if file_name =="v1 (legacy).js"
+        file_name = "v1.js"
+      end
       scorer.update( code:               File.readlines("./db/scorers/#{file_name}",'\n').join('\n'))
     end
   end
