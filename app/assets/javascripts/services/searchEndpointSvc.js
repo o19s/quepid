a4b24138c431c916874b2c745efddffe91a71257
Eric Pugh <epugh@opensourceconnections.com>
2025-03-03 13:03:23 -0500

    Enable archive/delete of search endpionts, polishing up the page

--- app/assets/javascripts/services/searchEndpointSvc.js
+++ app/assets/javascripts/services/searchEndpointSvc.js
@@ -167,5 +167,22 @@ angular.module('QuepidApp')
           });
       };
       
+      this.deleteSearchEndpoint = function(searchEndpointToDelete) {
+        var that        = this;
+        var self  = this;
+
+        return $http.delete('api/search_endpoints/' + searchEndpointToDelete.id)
+          .then(function() {
+            //that.refetchCaseLists();
+            var indexOfSearchEndpoint = self.searchEndpoints.indexOf( self.searchEndpoints.filter( function (item) {
+              return item.id === searchEndpointToDelete.id;
+            })[0] );
+            self.searchEndpoints.splice(indexOfSearchEndpoint, 1);            
+                      
+            broadcastSvc.send('updatedSearchEndpointsList', self.searchEndpoints);
+           
+          });
+      };
+      
     }
   ]);
