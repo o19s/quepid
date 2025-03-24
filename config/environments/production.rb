f9ea42b8caa9be98f84d28ccd00c9b0658c01cf3
Eric Pugh <epugh@opensourceconnections.com>
2025-03-24 14:08:20 -0400

    Does this work?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,7 @@ Rails.application.configure do
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: ENV.fetch('QUEPID_DOMAIN', nil) }
   
-  if Rails.configuration.prefer_ssl 
+  if config.prefer_ssl 
     config.action_mailer.default_url_options = { protocol: 'https' }
   end
 
