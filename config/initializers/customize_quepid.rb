01258c0e4f9c20a2fdae60f0cec2f847dd12e74d
Eric Pugh <epugh@opensourceconnections.com>
2025-03-24 14:22:33 -0400

    Once more into the breach

--- config/initializers/customize_quepid.rb
+++ config/initializers/customize_quepid.rb
@@ -20,7 +20,7 @@ Rails.application.config.quepid_version = ENV.fetch('QUEPID_VERSION', 'UNKNOWN')
 Rails.application.config.prefer_ssl = bool.deserialize(ENV.fetch('PREFER_SSL', false))
 
 if Rails.application.config.prefer_ssl 
-  Rails.application.config.action_mailer.default_url_options = { protocol: 'https' }
+  Rails.application.config.action_mailer.default_url_options[:protocol] = 'https' 
 end
 
 # == Quepid Default Scorer
