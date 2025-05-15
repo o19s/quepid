59d3d6e786f9970814557d3cdd2605e3e86f0ef2
Eric Pugh <epugh@opensourceconnections.com>
2025-05-15 15:08:16 -0400

    Now supporting books

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -8,37 +8,48 @@ module Api
       before_action :set_book, only: [ :show, :update, :destroy ]
       before_action :check_book, only: [ :show, :update, :destroy ]
 
-      def_param_group :book_params do
-        param :book, Hash, required: true do
-          param :name, String
-          param :show_rank, [ true, false ]
-          param :support_implicit_judgements, [ true, false ]
-          param :owner_id, Integer
-          param :scorer_id, Integer
-          param :selection_strategy_id, Integer
-        end
-      end
-
-      # @summary List all books to which the user has access.
       # @tags books
       def index
         @books = current_user.books_involved_with
         respond_with @books
       end
 
-      # @summary Get a book by id.
       # @tags books
       def show
         respond_with @book
       end
 
-      # @summary Create a Book
       # @tags books
-      # @request_body The book to be created. At least include an `name`. [!Book]
+      # @request_body
+      #   [
+      #     !Hash{
+      #       book: Hash{
+      #         name: String,
+      #         owner_id: !Integer,
+      #         scorer_id: !Integer,
+      #         selection_strategy_id: !Integer,
+      #         show_rank: Boolean,
+      #         support_implicit_judgements: Boolean
+      #       }
+      #     }
+      #   ]
+      # @request_body_example basic book [Hash]
+      #   {
+      #     book: {
+      #       name: "Oas",
+      #       show_rank: false,
+      #       support_implicit_judgements: false,
+      #       owner_id: 1,
+      #       scorer_id: 1,
+      #       selection_strategy_id: 1
+      #     }
+      #   }
       def create
         @book = Book.new(book_params)
-        team = Team.find_by(id: params[:book][:team_id])
-        @book.teams << team
+        if params[:book][:team_id]
+          team = Team.find_by(id: params[:book][:team_id])
+          @book.teams << team
+        end
         if @book.save
           respond_with @book
         else
@@ -46,9 +57,20 @@ module Api
         end
       end
 
-      # @summary A Book can be updated with this method
       # @tags books
-      # @request_body Book to be created [Hash{book: { name: String, show_rank: Boolean, support_implicit_judgements: Boolean, owner_id: !Integer, scorer_id: !Integer, selection_strategy_id: !Integer}}]
+      # @request_body
+      #   [
+      #     !Hash{
+      #       book: Hash{
+      #         name: String,
+      #         owner_id: !Integer,
+      #         scorer_id: !Integer,
+      #         selection_strategy_id: !Integer,
+      #         show_rank: Boolean,
+      #         support_implicit_judgements: Boolean
+      #       }
+      #     }
+      #   ]
       # @request_body_example basic book [Hash]
       #   {
       #     book: {
@@ -70,9 +92,8 @@ module Api
         end
       end
 
-      # @summary Delete a Book
       # @tags books
-      # Delete a book and its associated data like Query/Doc Pairs and Judgements.
+      # Delete a book and its child data including Query/Doc Pairs and Judgements.
       def destroy
         @book.really_destroy
 
