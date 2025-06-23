fac3b187471fddb7411611d8d44684aa488a6df0
Eric Pugh <epugh@opensourceconnections.com>
2025-06-23 14:20:36 -0400

    Vendor all the codemirror dependencies.

--- config/importmap.rb
+++ config/importmap.rb
@@ -13,19 +13,23 @@ pin 'ahoy', to: 'ahoy.js'
 pin 'js-cookie' # @3.0.5
 
 # CodeMirror 6 packages
-pin 'codemirror', to: 'https://ga.jspm.io/npm:codemirror@6.0.1/dist/index.js'
-pin '@codemirror/state', to: 'https://ga.jspm.io/npm:@codemirror/state@6.2.1/dist/index.js'
-pin '@codemirror/view', to: 'https://ga.jspm.io/npm:@codemirror/view@6.16.0/dist/index.js'
-pin '@codemirror/commands', to: 'https://ga.jspm.io/npm:@codemirror/commands@6.2.4/dist/index.js'
-pin '@codemirror/language', to: 'https://ga.jspm.io/npm:@codemirror/language@6.9.0/dist/index.js'
 pin '@codemirror/lang-javascript', to: 'https://ga.jspm.io/npm:@codemirror/lang-javascript@6.2.1/dist/index.js'
 pin '@codemirror/lang-json', to: 'https://ga.jspm.io/npm:@codemirror/lang-json@6.0.1/dist/index.js'
-pin '@codemirror/autocomplete', to: 'https://ga.jspm.io/npm:@codemirror/autocomplete@6.11.0/dist/index.js'
-pin '@lezer/common', to: 'https://ga.jspm.io/npm:@lezer/common@1.1.0/dist/index.js'
-pin '@lezer/highlight', to: 'https://ga.jspm.io/npm:@lezer/highlight@1.1.6/dist/index.js'
 pin '@lezer/javascript', to: 'https://ga.jspm.io/npm:@lezer/javascript@1.4.6/dist/index.js'
 pin '@lezer/lr', to: 'https://ga.jspm.io/npm:@lezer/lr@1.3.10/dist/index.js'
 pin '@lezer/json', to: 'https://ga.jspm.io/npm:@lezer/json@1.0.1/dist/index.js'
-pin 'style-mod', to: 'https://ga.jspm.io/npm:style-mod@4.1.0/src/style-mod.js'
-pin 'w3c-keyname', to: 'https://ga.jspm.io/npm:w3c-keyname@2.2.8/index.js'
 pin 'modules/editor', to: 'modules/editor.js'
+pin "codemirror" # @6.0.2
+pin "@codemirror/autocomplete", to: "@codemirror--autocomplete.js" # @6.18.6
+pin "@codemirror/commands", to: "@codemirror--commands.js" # @6.8.1
+pin "@codemirror/language", to: "@codemirror--language.js" # @6.11.1
+pin "@codemirror/lint", to: "@codemirror--lint.js" # @6.8.5
+pin "@codemirror/search", to: "@codemirror--search.js" # @6.5.11
+pin "@codemirror/state", to: "@codemirror--state.js" # @6.5.2
+pin "@codemirror/view", to: "@codemirror--view.js" # @6.37.2
+pin "@lezer/common", to: "@lezer--common.js" # @1.2.3
+pin "@lezer/highlight", to: "@lezer--highlight.js" # @1.2.1
+pin "@marijn/find-cluster-break", to: "@marijn--find-cluster-break.js" # @1.0.2
+pin "crelt" # @1.0.6
+pin "style-mod" # @4.1.2
+pin "w3c-keyname" # @2.2.8
