a3fc8f9baa0c8c1a4c3f25cac71d594547dcf1a3
Eric Pugh <epugh@opensourceconnections.com>
2025-06-23 16:16:29 -0400

    lint

--- config/importmap.rb
+++ config/importmap.rb
@@ -14,22 +14,22 @@ pin 'js-cookie' # @3.0.5
 
 # CodeMirror 6 packages
 pin 'modules/editor', to: 'modules/editor.js'
-pin "codemirror" # @6.0.2
-pin "@codemirror/commands", to: "@codemirror--commands.js" # @6.8.1
-pin "@codemirror/lint", to: "@codemirror--lint.js" # @6.8.5
-pin "@codemirror/search", to: "@codemirror--search.js" # @6.5.11
-pin "@codemirror/lang-json", to: "@codemirror--lang-json.js" # @6.0.2
-pin "@codemirror/language", to: "@codemirror--language.js" # @6.11.1
-pin "@codemirror/state", to: "@codemirror--state.js" # @6.5.2
-pin "@codemirror/view", to: "@codemirror--view.js" # @6.37.2
-pin "@lezer/common", to: "@lezer--common.js" # @1.2.3
-pin "@lezer/highlight", to: "@lezer--highlight.js" # @1.2.1
-pin "@lezer/json", to: "@lezer--json.js" # @1.0.3
-pin "@lezer/lr", to: "@lezer--lr.js" # @1.4.2
-pin "@marijn/find-cluster-break", to: "@marijn--find-cluster-break.js" # @1.0.2
-pin "crelt" # @1.0.6
-pin "style-mod" # @4.1.2
-pin "w3c-keyname" # @2.2.8
-pin "@codemirror/lang-javascript", to: "@codemirror--lang-javascript.js" # @6.2.4
-pin "@codemirror/autocomplete", to: "@codemirror--autocomplete.js" # @6.18.6
-pin "@lezer/javascript", to: "@lezer--javascript.js" # @1.5.1
+pin 'codemirror' # @6.0.2
+pin '@codemirror/commands', to: '@codemirror--commands.js' # @6.8.1
+pin '@codemirror/lint', to: '@codemirror--lint.js' # @6.8.5
+pin '@codemirror/search', to: '@codemirror--search.js' # @6.5.11
+pin '@codemirror/lang-json', to: '@codemirror--lang-json.js' # @6.0.2
+pin '@codemirror/language', to: '@codemirror--language.js' # @6.11.1
+pin '@codemirror/state', to: '@codemirror--state.js' # @6.5.2
+pin '@codemirror/view', to: '@codemirror--view.js' # @6.37.2
+pin '@lezer/common', to: '@lezer--common.js' # @1.2.3
+pin '@lezer/highlight', to: '@lezer--highlight.js' # @1.2.1
+pin '@lezer/json', to: '@lezer--json.js' # @1.0.3
+pin '@lezer/lr', to: '@lezer--lr.js' # @1.4.2
+pin '@marijn/find-cluster-break', to: '@marijn--find-cluster-break.js' # @1.0.2
+pin 'crelt' # @1.0.6
+pin 'style-mod' # @4.1.2
+pin 'w3c-keyname' # @2.2.8
+pin '@codemirror/lang-javascript', to: '@codemirror--lang-javascript.js' # @6.2.4
+pin '@codemirror/autocomplete', to: '@codemirror--autocomplete.js' # @6.18.6
+pin '@lezer/javascript', to: '@lezer--javascript.js' # @1.5.1
