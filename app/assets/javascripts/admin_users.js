04016b6e6d639d5f767631d24cc5eede846be39c
Eric Pugh <epugh@opensourceconnections.com>
2025-08-25 15:00:52 -0400

    Remove admin.js in favour of only loading cal heatmap in the admin_users.js file.

--- app/assets/javascripts/admin_users.js
+++ app/assets/javascripts/admin_users.js
@@ -1,2 +1,14 @@
+// Known issue that cal-heatmap (and d3) are super old and require old jquery.
+// Meanwhile, bootstrap/dist/js/bootstrap.bundle is Bootstrap 5, which doesn't not like
+// the version of jquery that we use.  So this means the dropdown in the header nav does not
+// work.  However, this is only on the /admin/users/3 show view, so we live with it.
+
+//= require jquery
+//  
+//= require bootstrap/dist/js/bootstrap.bundle
+
+//= require d3
+//= require cal-heatmap
+
 //= require admin/scorers
 //= require admin/user_pulse
