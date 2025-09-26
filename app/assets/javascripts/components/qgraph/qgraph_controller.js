d6ba772f798df40ee40fbce85c91cff32fabef38
Eric Pugh <epugh@opensourceconnections.com>
2025-09-26 18:42:56 -0400

    Showing annotations with no flicker

--- app/assets/javascripts/components/qgraph/qgraph_controller.js
+++ app/assets/javascripts/components/qgraph/qgraph_controller.js
@@ -47,50 +47,59 @@ angular.module('QuepidApp')
       ctrl.render = renderGraph;
 
       function renderGraph() {
-        if (ctrl.scores) {
-          $scope.graph.select('line').remove();
-          $scope.graph.select('path').remove();
+        // Ensure all required data is available
+        if (!ctrl.scores || !ctrl.max || !$scope.graph || ctrl.width <= 0 || ctrl.height <= 0) {
+          return;
+        }
 
-          // NOTE: slice is doing two jobs here:
-          //     1. limit the number of points in the graph.
-          //     2. create a shallow copy of the array.
-          //     if you alter this, make sure 2 happens or there will be woe
-          var data = ctrl.scores.slice(-10).sort(function (a, b) {
-            return d3.ascending(a.updated_at, b.updated_at);
+        // Remove existing elements
+        $scope.graph.selectAll('path').remove();
+        $scope.graph.selectAll('.marker').remove();
+
+        // NOTE: slice is doing two jobs here:
+        //     1. limit the number of points in the graph.
+        //     2. create a shallow copy of the array.
+        //     if you alter this, make sure 2 happens or there will be woe
+        var data = ctrl.scores.slice(-10).sort(function (a, b) {
+          return d3.ascending(a.updated_at, b.updated_at);
+        });
+
+        if (data.length === 0) {
+          return;
+        }
+
+        var x = d3.scaleLinear().domain([0, (data.length - 1)]).range([0, ctrl.width]);
+        var y = d3.scaleLinear().domain([0, ctrl.max]).range([ctrl.height, 0]);
+
+        var line = d3.line()
+          .x(function (d, i) {
+            return x(i) + ctrl.margin.left;
+          })
+          .y(function (d) {
+            return y(d.score);
           });
 
-          var x = d3.scaleLinear().domain([0, (data.length - 1)]).range([0, ctrl.width]);
-          var y = d3.scaleLinear().domain([0, ctrl.max]).range([ctrl.height, 0]);
+        // First, add the path
+        $scope.graph.append('path').attr('d', line(data));
 
-          var marker = function (xpos, note) {
+        // Then, add markers for annotations (on top of the path)
+        data.forEach(function(d, i) {
+          if (d.note) {
+            var xpos = x(i) + ctrl.margin.left;
             $scope.graph.append('line')
+              .attr('class', 'marker')
               .attr('x1', xpos)
               .attr('x2', xpos)
               .attr('y1', 0)
               .attr('y2', ctrl.height)
               .attr('class', 'marker')
-              .on('mouseover', function (event) { $scope.tip.show(note, this, event); })
+              .style('pointer-events', 'all')
+              .on('mouseover', function (event) { $scope.tip.show(d.note, this, event); })
               .on('mouseout', function () { $scope.tip.hide(); })
               .append('title')
-              .text(note);
-          };
-
-          var line = d3.line()
-            .x(function (d, i) {
-              var xpos = x(i) + ctrl.margin.left;
-
-              if (d.note) {
-                marker(xpos, d.note);
-              }
-
-              return xpos;
-            })
-            .y(function (d) {
-              return y(d.score);
-            });
-
-          $scope.graph.append('path').attr('d', line(data));
-        }
+              .text(d.note);
+          }
+        });
       }
     }
   ]);
