01fb5ec35a33b35132c10d0f452006d1f31f1394
Eric Pugh <epugh@opensourceconnections.com>
2025-09-26 21:33:29 -0400

    back in business!

--- app/assets/javascripts/admin_users.js
+++ app/assets/javascripts/admin_users.js
@@ -1,10 +1,4 @@
-// Known issue that cal-heatmap (and d3) are super old and require old jquery.
-// Meanwhile, bootstrap/dist/js/bootstrap.bundle is Bootstrap 5, which doesn't not like
-// the version of jquery that we use.  So this means the dropdown in the header nav does not
-// work.  However, this is only on the /admin/users/3 show view, so we live with it.
 
-//= require jquery
-//  
 //= require bootstrap/dist/js/bootstrap.bundle
 
 // We include modern D3 via node_modules
