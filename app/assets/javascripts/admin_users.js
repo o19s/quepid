ef3176f0a71bdf94fb896ae5e52bd21934050122
Eric Pugh <epugh@opensourceconnections.com>
2025-09-26 21:38:12 -0400

    more clean up

--- app/assets/javascripts/admin_users.js
+++ app/assets/javascripts/admin_users.js
@@ -1,8 +1,5 @@
-
 //= require bootstrap/dist/js/bootstrap.bundle
 
 // We include modern D3 via node_modules
 //= require d3/dist/d3
 //= require cal-heatmap/dist/cal-heatmap.min
-
-//= require admin/scorers
