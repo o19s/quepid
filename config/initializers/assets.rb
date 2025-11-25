11ea495a57d1d249a66b479859d9cc2ac32ca33c
Eric Pugh <epugh@opensourceconnections.com>
2025-11-25 13:19:00 -0500

    Deal with build issues in prod mode.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,11 +8,14 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add node_modules to asset paths for Propshaft to serve
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# We no longer add the entire node_modules tree; build scripts copy needed assets
+Rails.application.config.assets.paths << Rails.root.join('app/assets/builds')
 
 # Add Karma test files in development/test
 Rails.application.config.assets.paths << Rails.root.join('spec/karma') if Rails.env.local?
 
 # Add Bootstrap Icons font directory
 Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap-icons/font')
+
+# NOTE: Avoid scanning packages with broken symlinks (e.g. angular-flash) by not
+# adding node_modules wholesale. Specific subpaths like fonts are added above.
