340a6176280cedee3d4ba08ab5429959f477a82e
Eric Pugh <epugh@opensourceconnections.com>
2026-01-08 12:33:45 -0500

    Implement David's optimistic/pessimistic logic.
    See https://quepid-docs.dev.o19s.com/2/quepid/63/how-judgements-are-averaged-into-a-rating-in-a-case

--- app/services/ratings_manager.rb
+++ app/services/ratings_manager.rb
@@ -33,7 +33,6 @@ class RatingsManager
 
   # rubocop:disable Metrics/AbcSize
   # rubocop:disable Metrics/MethodLength
-  # rubocop:disable Metrics/PerceivedComplexity
   def sync_judgements_to_ratings kase, query_doc_pair
     @counter += 1
     query = Query.find_or_initialize_by(case: kase, query_text: query_doc_pair.query_text)
@@ -58,32 +57,50 @@ class RatingsManager
     # )
     broadcast_case_specific_notification kase, query_doc_pair, @book.query_doc_pairs.size, @counter
 
-    count_of_judgements = query_doc_pair.judgements.rateable.size
+    judgements = query_doc_pair.judgements.rateable
 
     # only calculate this if we have valid judgements.  If everything is unrateable, then don't proceed.
-    if count_of_judgements.positive?
-      summed_rating = query_doc_pair.judgements.rateable.sum(&:rating)
+    if judgements.any?
+      calculated_rating = calculate_rating_from_judgements(judgements)
 
       rating = Rating.find_or_initialize_by(query: query, doc_id: query_doc_pair.doc_id)
-      # I think the whole rating having a user was bogus becasue we have multiple raters
-      # on the book side, which kind of messes this us.
-      # rating.user = query_doc_pair.judgements.last.user if rating.user.nil?
       rating.rating = if @book.support_implicit_judgements?
-                        summed_rating / count_of_judgements
+                        calculated_rating
                       else
                         # explicit judgements only work with integers.
-                        (summed_rating / count_of_judgements).round
+                        calculated_rating.round
                       end
 
       @ratings_created += 1 if rating.new_record?
 
       rating.save
-
     end
   end
+
   # rubocop:enable Metrics/AbcSize
   # rubocop:enable Metrics/MethodLength
-  # rubocop:enable Metrics/PerceivedComplexity
+  # Calculates a rating from multiple judgements using an optimistic-pessimistic approach:
+  # 1. Take the three highest ratings (optimistic: assume the best judges rated highest)
+  # 2. If those judges agree, use that value
+  # 3. If they disagree, use the minimum of the top three (pessimistic: trust the lower rating,
+  #    assuming judges tend to overrate)
+  #
+  # @param judgements [ActiveRecord::Relation] Collection of rateable judgements
+  # @return [Float] The calculated rating
+  def calculate_rating_from_judgements judgements
+    ratings = judgements.map(&:rating).sort.reverse
+
+    # Take the top 3 ratings (or fewer if less than 3 exist)
+    top_ratings = ratings.first(3)
+
+    # If all top ratings agree, use that value
+    # Otherwise, use the minimum (trust the pessimistic judge)
+    if 1 == top_ratings.uniq.size
+      top_ratings.first
+    else
+      top_ratings.min
+    end
+  end
 
   def broadcast_case_specific_notification acase, query_doc_pair, query_doc_pair_count, counter
     Turbo::StreamsChannel.broadcast_render_to(
