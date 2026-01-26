9cbc88b6d72df25a0c7cc3fff170554a204078b7
Eric Pugh <epugh@opensourceconnections.com>
2026-01-26 09:53:52 -0500

    white space!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  #config.logger   = ActiveSupport::TaggedLogging.logger($stdout)
+  # config.logger   = ActiveSupport::TaggedLogging.logger($stdout)
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info')
