61ff89f8e62b3f3d801bcbb51d1742a0c055cf94
Eric Pugh <epugh@opensourceconnections.com>
2026-02-16 11:36:53 -0500

    rubocop

--- config/importmap.rb
+++ config/importmap.rb
@@ -25,21 +25,21 @@ pin '@popperjs/core', to: 'popper.min.js' # @2.11.8
 # CodeMirror 6 packages
 pin 'modules/editor', to: 'modules/editor.js'
 pin 'codemirror' # @6.0.2
-pin "@codemirror/commands", to: "@codemirror--commands.js" # @6.10.2
-pin "@codemirror/lint", to: "@codemirror--lint.js" # @6.9.4
-pin "@codemirror/search", to: "@codemirror--search.js" # @6.6.0
+pin '@codemirror/commands', to: '@codemirror--commands.js' # @6.10.2
+pin '@codemirror/lint', to: '@codemirror--lint.js' # @6.9.4
+pin '@codemirror/search', to: '@codemirror--search.js' # @6.6.0
 pin '@codemirror/lang-json', to: '@codemirror--lang-json.js' # @6.0.2
-pin "@codemirror/language", to: "@codemirror--language.js" # @6.12.1
-pin "@codemirror/state", to: "@codemirror--state.js" # @6.5.4
-pin "@codemirror/view", to: "@codemirror--view.js" # @6.39.14
-pin "@lezer/common", to: "@lezer--common.js" # @1.5.1
-pin "@lezer/highlight", to: "@lezer--highlight.js" # @1.2.3
+pin '@codemirror/language', to: '@codemirror--language.js' # @6.12.1
+pin '@codemirror/state', to: '@codemirror--state.js' # @6.5.4
+pin '@codemirror/view', to: '@codemirror--view.js' # @6.39.14
+pin '@lezer/common', to: '@lezer--common.js' # @1.5.1
+pin '@lezer/highlight', to: '@lezer--highlight.js' # @1.2.3
 pin '@lezer/json', to: '@lezer--json.js' # @1.0.3
-pin "@lezer/lr", to: "@lezer--lr.js" # @1.4.8
-pin "@marijn/find-cluster-break", to: "@marijn--find-cluster-break.js" # @1.0.2
-pin "crelt" # @1.0.6
-pin "style-mod" # @4.1.3
-pin "w3c-keyname" # @2.2.8
+pin '@lezer/lr', to: '@lezer--lr.js' # @1.4.8
+pin '@marijn/find-cluster-break', to: '@marijn--find-cluster-break.js' # @1.0.2
+pin 'crelt' # @1.0.6
+pin 'style-mod' # @4.1.3
+pin 'w3c-keyname' # @2.2.8
 pin '@codemirror/lang-javascript', to: '@codemirror--lang-javascript.js' # @6.2.4
 pin '@codemirror/autocomplete', to: '@codemirror--autocomplete.js' # @6.20.0
 pin '@lezer/javascript', to: '@lezer--javascript.js' # @1.5.4
