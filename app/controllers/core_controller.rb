644c5280f2be31b976559298f83cf3671d417c75
Dave Mackey <dave@davemackey.net>
2026-03-20 16:11:21 -0400

    Replace Angular header dropdowns with server-rendered ERB + BS5
    Remove HeaderCtrl, uib-dropdown, ng-repeat, and <new-case> component
    from the core header. Dropdowns now use data-bs-toggle (Bootstrap 5 JS)
    and render recent cases/books server-side via ERB. Case counts and
    action buttons match the Angular version.

--- app/controllers/core_controller.rb
+++ app/controllers/core_controller.rb
@@ -7,6 +7,11 @@ class CoreController < ApplicationController
 
   def index
     Analytics::Tracker.track_user_swapped_protocol current_user, @case, params['protocolToSwitchTo'] if params['protocolToSwitchTo']
+
+    @recent_cases = recent_cases 4
+    @recent_cases_count = current_user.cases_involved_with.not_archived.count
+    @recent_books = recent_books 4
+    @recent_books_count = current_user.books_involved_with.count
   end
 
   def new
