6bfe78c3bcfdc88732b2e7f677953344a517c816
Eric Pugh <epugh@opensourceconnections.com>
2026-05-28 10:07:42 -0400

    turns out order of operations loading in staging and prod are different

--- config/application.rb
+++ config/application.rb
@@ -46,6 +46,14 @@ module Quepid
     # Maybe in Quepid 9?
     config.active_record.encryption.support_unencrypted_data = true
 
+    # Encryption keys must be set here (not in config/initializers/) so they are in
+    # place before the active_record.encryption Railtie initializer copies them into
+    # ActiveRecord::Encryption.config. We provide defaults, but you should set your
+    # own keys via env vars in production and NOT lose them.
+    config.active_record.encryption.primary_key         = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY',         'bnYX3NlvUJxHWXwNYBgP33yi8BKlN7Ml')
+    config.active_record.encryption.deterministic_key   = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY',   'OItaH6HSftjoxkl9QDejPAmQ8EaFOlwk')
+    config.active_record.encryption.key_derivation_salt = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT', 'BzPDVAl1jAUquD4p7rM9J40wAwf7CCFh')
+
     # == SSL Specific Settings
     # Note, if true then this will allow Quepid to ONLY talk to HTTPS based search engines.
     config.force_ssl = true if 'true' == ENV['FORCE_SSL']
