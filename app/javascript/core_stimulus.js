f876f35eea9e2d47ab13cd1f3163473c29967c4e
Dave Mackey <dmackey@opensourceconnections.com>
2026-09-15 09:45:08 -0400

    Migrate clone-case modal to Stimulus, remove Angular component
    - Wire clone-case-core into the case toolbar via the shared
      clone_case_core_controller.js + _clone_case_core_modal.html.erb pattern
      (fetches try history, posts to api/clone/cases, then navigates to the new
      case), and delete the Angular <clone-case> component and caseSvc.cloneCase.

--- app/javascript/core_stimulus.js
+++ app/javascript/core_stimulus.js
@@ -9,10 +9,12 @@
 import { application } from "controllers/application"
 import ShareCaseCoreController from "controllers/share_case_core_controller"
 import DeleteCaseOptionsCoreController from "controllers/delete_case_options_core_controller"
+import CloneCaseCoreController from "controllers/clone_case_core_controller"
 import BsTooltipController from "controllers/bs_tooltip_controller"
 import BsPopoverController from "controllers/bs_popover_controller"
 
 application.register("share-case-core", ShareCaseCoreController)
 application.register("delete-case-options-core", DeleteCaseOptionsCoreController)
+application.register("clone-case-core", CloneCaseCoreController)
 application.register("bs-tooltip", BsTooltipController)
 application.register("bs-popover", BsPopoverController)
