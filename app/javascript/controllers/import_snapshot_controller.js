111f811675c22883d03668bef0f0fff6fe333861
Dave Mackey <dmackey@opensourceconnections.com>
2026-09-15 09:56:29 -0400

    Send snapshot imports sequentially to avoid a find-or-create deadlock

--- app/javascript/controllers/import_snapshot_controller.js
+++ app/javascript/controllers/import_snapshot_controller.js
@@ -201,7 +201,6 @@ export default class extends Controller {
     // server-rendered URL to pass in - fall back to the root URL.
     const url = `${getQuepidRootUrl()}/api/cases/${caseId}/snapshots/imports`
 
-
     const response = await apiFetch(url, {
       method: 'POST',
       headers: {
